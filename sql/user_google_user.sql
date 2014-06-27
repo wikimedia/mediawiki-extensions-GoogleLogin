69d1894dbc056603c36ad939330ddf4e39959170
Antoine Musso <hashar@free.fr>
2014-06-27 17:51:11 +0200

    Drop index from primary key
    A primary key having an index, there is no need to create another index
    for it.
    
    Additionally, the index had the same name as the table, which cause an
    error under sqlite.
    
    Change-Id: Ie537c54e6e169fe5cbacbae4fd0d85ffa245eedb

--- sql/user_google_user.sql
+++ sql/user_google_user.sql
@@ -5,6 +5,3 @@ CREATE TABLE /*$wgDBprefix*/user_google_user (
   user_googleid DECIMAL(25,0) unsigned NOT NULL PRIMARY KEY,
   user_id int(10) unsigned NOT NULL
 ) /*$wgDBTableOptions*/;
-
-
-CREATE INDEX /*$wgDBprefix*/user_google_user ON /*$wgDBprefix*/user_google_user(user_id);
