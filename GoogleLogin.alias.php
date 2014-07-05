5b3338dd60c79b01d5c6b08aa4cbb87018b10550
florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-07-05 20:17:23 +0200

    Move Alias back to root
    Move GoogleLogin.alias.php from includes/ back to root.
    
    Change-Id: I8f52378fd79728faeaa2fb0abc306f7879b14e56

--- GoogleLogin.alias.php
+++ GoogleLogin.alias.php
@@ -1,4 +1,33 @@
-
+<?php
+/**
+ * Aliases for Google Login
+ * @ingroup Extensions
+ */
 // @codingStandardsIgnoreFile
 
 $specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'GoogleLogin' => array( 'GoogleLogin', 'Login with Google' ),
+);
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = array(
+	'GoogleLogin' => array( 'НајаваСоGoogle' ),
+);
+
+/** Malay (Bahasa Melayu) */
+$specialPageAliases['ms'] = array(
+	'GoogleLogin' => array( 'Log_masuk_google', 'Log_masuk_dengan_Google' ),
+);
+
+/** Simplified Chinese (中文（简体）‎) */
+$specialPageAliases['zh-hans'] = array(
+	'GoogleLogin' => array( 'Google登录' ),
+);
+
+/** Traditional Chinese (中文（繁體）‎) */
+$specialPageAliases['zh-hant'] = array(
+	'GoogleLogin' => array( 'Google登入' ),
+);
