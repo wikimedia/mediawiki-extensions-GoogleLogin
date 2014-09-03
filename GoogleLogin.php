37b65dbcc5308b94600d5df1839ccbb163841867
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-09-03 09:30:26 +0200

    Version jump
    Change-Id: I1d87cb751a2d541e4fe154e165a9a9f90a23833a

--- GoogleLogin.php
+++ GoogleLogin.php
@@ -32,7 +32,7 @@
 		'author' => 'Florian Schmidt',
 		'url' => 'https://www.mediawiki.org/wiki/Extension:GoogleLogin',
 		'descriptionmsg' => 'googlelogin-desc',
-		'version'  => '0.1.3-git',
+		'version'  => '0.2.0',
 		'license-name' => "MIT",
 	);
 
