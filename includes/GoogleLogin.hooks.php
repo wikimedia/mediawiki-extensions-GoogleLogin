af2aa3a2543feaf725e655263b2e003863463126
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-11-03 18:17:47 +0100

    Fix: undefined var
    Change-Id: I787861db812725be0cafced376fbd2d835c5187d
    (cherry picked from commit 1ec153d67a4de4b70d79f3f2f4ce564a5b322f96)

--- includes/GoogleLogin.hooks.php
+++ includes/GoogleLogin.hooks.php
@@ -92,6 +92,7 @@
 				}
 
 				// Replace login link with GoogleLogin link
+				$googleLogin = new GoogleLogin;
 				$personal_urls['login']['text'] = wfMessage( 'googlelogin' )->text();
 				$personal_urls['login']['href'] = $googleLogin->getLoginUrl( $skin, $title );
 			}
