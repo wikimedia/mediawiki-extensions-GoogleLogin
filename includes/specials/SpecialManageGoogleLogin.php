74f8e714b5883e1c676dd07c196b20e0d85f62a2
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-05-17 00:58:02 +0200

    WIP: Rewrite GoogleLogin to be a PrimaryAuthenticationProvider
    This change implements:
     * A new primarey authentication provider (GooglePrimaryAuthenticationProvider),
       which let the user authenticate with the Google account.
     * A workaround for the requirement of a static redirect uri of Google
       (including an own CSRF protection using Google's state field) using a new
       Special page (SPecial:GoogleLoginReturn) which handles the required
       checks for all data and redirects to Special:UserLogin with the correct
       original csrf token of the user login special page.
     * Reworks Special:GoogleLogin to be an information special page only (with
       links to link and unlink)
     * Reworked GoogleUser as a static class (which also doesn't inherit from User
       anymore) for:
       * Get an User object from a Google ID
       * Get a Google ID for a User object
       * Connect an User object with a Google ID
       * Terminate a connection between a Google ID and an User
       * Check, if a Google ID is already connected or not
     * Reworked GoogleLogin.body.php (GoogleLogin class) to remove unnecessary functions,
       and work with new GoogleUser object.
     * Cleaned up GoogleLogin.hooks.php to remove unnecessary hooks
     * Cleaned up extension.json to remove definition for unused hooks
     * Put any class (except Api) into GoogleLogin namespaces
     * Reworks how Google account connections are handled. Now the user can have
       as much Google accounts connected with the wiki account as he wants. This
       changes the 1:1 relationship (used until yet) to a 1:n relationship.
     * Added a SQL patch to remove the primary key from user_googleid in the
       user_google_user table (needed for multiple Google account connections)
    
    This currently breaks the login with the so far used special page GoogleLogin,
    see the todos.
    
    Bug: T110294
    Change-Id: I0d0447eb35ff38fab8bd4d945469cd66cabe0f73

--- includes/specials/SpecialManageGoogleLogin.php
+++ includes/specials/SpecialManageGoogleLogin.php
@@ -1,10 +1,18 @@
 <?php
-use GoogleLogin\GoogleUser as User;
+
+namespace Googlelogin\Specials;
+
+use SpecialPage;
+use Html;
+use HTMLForm;
+use User;
+use ConfigFactory;
+use Http;
+use ErrorPageError;
+
+use GoogleLogin\GoogleUser;
 
 class SpecialManageGoogleLogin extends SpecialPage {
-	/** @var $mGoogleLogin saves an instance of GoogleLogin class */
-	private $mGoogleLogin;
-
 	/** @var User $manageableUser User object of the user to manage */
 	private $manageableUser = null;
 
@@ -74,7 +82,6 @@ class SpecialManageGoogleLogin extends SpecialPage {
 	 * @param User $user
 	 */
 	private function manageUser( User $user ) {
-		$request = $this->getRequest();
 		$out = $this->getOutput();
 
 		$out->addModules(
@@ -84,100 +91,56 @@ class SpecialManageGoogleLogin extends SpecialPage {
 			]
 		);
 		$out->addBackLinkSubtitle( $this->getPageTitle() );
-		$id = $user->getGoogleId();
-		$googleId = $request->getVal( 'googleid' );
-		// try to create a new GoogleUser object with the given id to check, if there's
-		// already an user with this google id.
-		$newGoogleUser = User::newFromGoogleId( $googleId );
-		$terminateLink = $request->getVal( 'terminate-link' );
-		if ( isset( $terminateLink ) && $user->hasConnectedGoogleAccount() ) {
-			// terminate the connection
-			if ( $user->terminateGoogleConnection() ) {
-				$out->addWikiMsg( 'googlelogin-manage-terminatesuccess' );
-				$id = false;
-			} else {
-				$out->addWikiMsg( 'googlelogin-manage-changederror' );
-			}
-		} elseif ( $googleId ) {
-			if ( !is_numeric( $googleId ) ) {
-				$out->wrapWikiMsg( '<div class="error">$1</div>', 'googlelogin-manage-invalidid' );
-			} elseif ( $newGoogleUser->getId() !== 0 ) {
-				// if the ID is already given to another user, it can't be associtated with this user
-				$out->wrapWikiMsg( '<div class="error">$1</div>', 'googlelogin-manage-givenid' );
-			} else {
-				// check, if the google id has a google plus profile
-				$glConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
-				$plusCheck = Http::get(
-					'https://www.googleapis.com/plus/v1/people/' .
-					$googleId .
-					'?key=' .
-					$glConfig->get( 'GLAPIKey' )
-				);
-				if ( !$plusCheck ) {
-					// it seems, that the google id doesn't have a plus profile
-					// (or another error occur). Add a notice message for it.
-					$out->addWikiMsg( 'googlelogin-manage-noplus' );
-				}
 
-				if ( $user->connectWithGoogle( $googleId ) ) {
-					$out->addWikiMsg( 'googlelogin-manage-changedsuccess' );
-					$id = $user->getGoogleId();
-				} else {
-					$out->addWikiMsg( 'googlelogin-manage-changederror' );
-				}
-			}
-		}
 		$out->addWikiMsg( 'googlelogin-manage-user', $user->getName() );
-		if ( $id ) {
-			$out->addHtml(
+		$googleIds = GoogleUser::getGoogleIdFromUser( $user );
+		if ( $googleIds ) {
+			$out->addHTML(
 				Html::openElement( 'div' ) .
 				$this->msg( 'googlelogin-manage-linked' )->escaped() .
-				Html::openElement( 'strong' ) .
-				Html::element( 'span',
-					[
-						'class' => 'googlelogin-googleid',
-					],
-					$id
-				) .
-				Html::element( 'a',
-					[
-						'href' => 'javascript:void(0)',
-						'class' => 'googlelogin-googleid hidden',
-						'data-googleid' => $id,
-					],
-					$id
-				) .
+				Html::openElement( 'strong' )
+			);
+			
+			foreach ($googleIds as $count => $googleId) {
+				if ( $count !== 0 ) {
+					$out->addHTML( ', ' );
+				}
+				$out->addHTML(
+					Html::element( 'a',
+						[
+							'href' => 'javascript:void(0)',
+							'data-googleid' => $googleId,
+						],
+						$googleId
+					) );
+			}
+			$out->addHTML(
 				Html::closeElement( 'strong' ) .
 				Html::closeElement( 'div' )
 			);
-			$formId = $id;
 		} else {
 			$out->addWikiMsg( 'googlelogin-manage-notlinked' );
-			$formId = '';
 		}
-		$formFields = [
-			'googleid' => [
-				'type' => 'text',
-				'name' => 'googleid',
-				'label-raw' => 'Google-ID:',
-				'default' => $formId,
+		$formFields = [];
+		if ( $googleIds ) {
+			$formFields['googleids'] =  [
+				'type' => 'checkmatrix',
+				'columns' => [
+					$this->msg( 'googlelogin-manage-terminatebutton' )->escaped() => 'google'
+				],
+				'rows' => array_combine( $googleIds, $googleIds ),
+				'label-raw' => 'Google ID',
 				'id' => 'mw-gl-username',
-			]
+			];
+		}
+		$formFields['addgoogleid'] = [
+			'type' => 'text',
+			'label-message' => 'googlelogin-manage-addlabel',
 		];
 		$htmlForm = HTMLForm::factory( 'ooui', $formFields, $this->getContext(), 'googlelogin-change' );
 		$htmlForm->addHiddenField( 'username', $user->getName() );
 		$htmlForm->setWrapperLegendMsg( $this->msg( 'googlelogin-manage-changelegend' ) );
 		$htmlForm->setSubmitCallback( [ $this, 'submitForm' ] );
-		if ( $id ) {
-			$htmlForm->addButton(
-				'terminate-link',
-				$this->msg( 'googlelogin-manage-terminatebutton' )->escaped(),
-				null,
-				[
-					'flags' => [ 'destructive' ],
-				]
-			);
-		}
 		$htmlForm->show();
 	}
 
@@ -191,11 +154,68 @@ class SpecialManageGoogleLogin extends SpecialPage {
 	public function submitForm( array $data, $checkSession = true ) {
 		$user = $this->getUser();
 		$request = $this->getRequest();
+		$out = $this->getOutput();
 		$name = ( isset( $data['username'] ) ? $data['username'] : '' );
 		if ( $checkSession && !$user->matchEditToken( $request->getVal( 'wpEditToken' ), $name ) ) {
 			throw new ErrorPageError( 'sessionfailure-title', 'sessionfailure' );
 		}
-		$this->manageableUser = User::newFromName( $name );
+		if ( $name ) {
+			$this->manageableUser = User::newFromName( $name );
+		}
+		if ( !$this->manageableUser ) {
+			return false;
+		}
+		$requestAddGoogleId = null;
+		$requestGoogleId = null;
+		if ( isset( $data['googleids'] ) ) {
+			$requestGoogleId = $data['googleids'];
+		}
+		if ( isset( $data['addgoogleid'] ) ) {
+			$requestAddGoogleId = $data['addgoogleid'];
+		}
+		if ( isset( $requestGoogleId ) && GoogleUser::hasConnectedGoogleAccount( $user ) ) {
+			// terminate the connection
+			foreach ( $requestGoogleId as $count => $googleId ) {
+				$id = str_replace( 'google-', '', $googleId );
+				if ( GoogleUser::terminateGoogleConnection( $user, $id ) ) {
+					$out->addWikiMsg( 'googlelogin-manage-terminatesuccess' );
+				} else {
+					$out->addWikiMsg( 'googlelogin-manage-changederror' );
+				}
+			}
+		}
+
+		if ( $requestAddGoogleId ) {
+			// try to create a new GoogleUser object with the given id to check, if there's
+			// already an user with this google id.
+			$newGoogleUser = GoogleUser::isGoogleIdFree( $requestAddGoogleId );
+			if ( !is_numeric( $requestAddGoogleId ) ) {
+				$out->wrapWikiMsg( '<div class="error">$1</div>', 'googlelogin-manage-invalidid' );
+			} elseif ( !$newGoogleUser ) {
+				// if the ID is already given to another user, it can't be associtated with this user
+				$out->wrapWikiMsg( '<div class="error">$1</div>', 'googlelogin-manage-givenid' );
+			} else {
+				// check, if the google id has a google plus profile
+				$glConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
+				$plusCheck = Http::get(
+					'https://www.googleapis.com/plus/v1/people/' .
+					$requestAddGoogleId .
+					'?key=' .
+					$glConfig->get( 'GLAPIKey' )
+				);
+				if ( !$plusCheck ) {
+					// it seems, that the google id doesn't have a plus profile
+					// (or another error occur). Add a notice message for it.
+					$out->addWikiMsg( 'googlelogin-manage-noplus' );
+				}
+
+				if ( GoogleUser::connectWithGoogle( $user, $requestAddGoogleId ) ) {
+					$out->addWikiMsg( 'googlelogin-manage-changedsuccess' );
+				} else {
+					$out->addWikiMsg( 'googlelogin-manage-changederror' );
+				}
+			}
+		}
 		return false;
 	}
 
--- includes/sql/user_google_user.sql
+++ includes/sql/user_google_user.sql
@@ -2,6 +2,6 @@
 -- extension Google Login SQL schema
 --
 CREATE TABLE /*$wgDBprefix*/user_google_user (
-  user_googleid DECIMAL(25,0) unsigned NOT NULL PRIMARY KEY,
+  user_googleid DECIMAL(25,0) unsigned NOT NULL,
   user_id int(10) unsigned NOT NULL
 ) /*$wgDBTableOptions*/;
