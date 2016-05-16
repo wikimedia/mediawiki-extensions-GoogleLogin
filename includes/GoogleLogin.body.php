fdbdcaf9009d4cea91b8fa3e015c50cb272dfc7f
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-05-17 00:58:02 +0200

    Rewrite GoogleLogin to be a PrimaryAuthenticationProvider
    This change implements:
     * A new primarey authentication provider (GooglePrimaryAuthenticationProvider),
       which let the user authenticate with the Google account.
     * A workaround for the requirement of a static redirect uri of Google
       (including an own CSRF protection using Google's state field) using a new
       Special page (Special:GoogleLoginReturn) which handles the required
       checks for all data and redirects to Special:UserLogin with the correct
       original csrf token of the user login special page.
     * Reworks Special:GoogleLogin to be an information special page only (with
       links to link and unlink)
     * Reworked GoogleUser as a static class (which also doesn't inherit from User
       anymore) for:
       * Get an User object from a Google ID
       * Get a Google ID for a User object
       * Connect an User object with a Google ID
       * Terminate a connection between a Google ID and an User
       * Check, if a Google ID is already connected or not
     * Reworked GoogleLogin.body.php (GoogleLogin class) to remove unnecessary functions,
       and work with new GoogleUser object.
     * Cleaned up GoogleLogin.hooks.php to remove unnecessary hooks
     * Cleaned up extension.json to remove definition for unused hooks
     * Removed now unused ResourceLoader modules
     * Added a Google icon and a style module that styles the login/create with Google button
       on Special:UserLogin and Special:CreateAccount according to the brand guidelines of
       Google
     * Put any class (except Api) into GoogleLogin namespaces
     * Reworks how Google account connections are handled. Now the user can have
       as much Google accounts connected with the wiki account as he wants. This
       changes the 1:1 relationship (used until yet) to a 1:n relationship.
     * Added a SQL patch to add an index for the user_id field in the
       user_google_user table
    
    Bug: T110294
    Change-Id: I0d0447eb35ff38fab8bd4d945469cd66cabe0f73

--- includes/GoogleLogin.body.php
+++ includes/GoogleLogin.body.php
@@ -1,34 +1,36 @@
 <?php
-use GoogleLogin\GoogleUser as User;
-use \User as MWUser;
 
-class GoogleLogin extends ContextSource {
-	/** @var $mSpecialPageTitle stores the Title object for GL special page */
-	private $mSpecialPageTitle;
-	/** @var $mRequestToken saves the request token for forms created with this instance */
-	private $mRequestToken;
+namespace GoogleLogin;
+
+use ConfigFactory;
+
+use Google_Client;
+
+class GoogleLogin {
 	/** @var $mGoogleClient Stores an instance of GoogleClient */
-	private $mGoogleClient;
-	/** @var $mPlusClient Stores an instance of Google Service Plus */
-	private $mPlusClient;
-	/** @var $mHost The Host of E-Mail provided by Google */
-	private $mHost;
+	private static $mGoogleClient;
 	/** @var $mConfig Config object created for GoogleLogin extension */
-	private $mConfig;
+	private static $mConfig;
+	/** @var $mHost The Host of E-Mail provided by Google */
+	private static $mHost;
 
 	/**
 	 * Returns an prepared instance of Google client to do requests with to Google API
 	 * @return Google_Client
 	 */
-	public function getClient() {
-		if ( empty( $this->mGoogleClient ) ) {
-			$client = $this->includeAPIFiles();
-			$this->mGoogleClient = $this->prepareClient(
-				$client,
-				$this->getSpecialPageUri()
-			);
+	public static function getClient( $returnToUrl, $token ) {
+		if ( empty( self::$mGoogleClient ) ) {
+			$glConfig = self::getGLConfig();
+			$client = new Google_Client();
+			$client->setClientId( $glConfig->get( 'GLAppId' ) );
+			$client->setClientSecret( $glConfig->get( 'GLSecret' ) );
+			$client->setRedirectUri( $returnToUrl );
+			$client->addScope( 'profile' );
+			$client->addScope( 'email' );
+			$client->setState( $token );
+			self::$mGoogleClient = $client;
 		}
-		return $this->mGoogleClient;
+		return self::$mGoogleClient;
 	}
 
 	/**
@@ -36,176 +38,11 @@ class GoogleLogin extends ContextSource {
 	 *
 	 * @return Config
 	 */
-	public function getGLConfig() {
-		if ( $this->mConfig === null ) {
-			$this->mConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
+	public static function getGLConfig() {
+		if ( self::$mConfig === null ) {
+			self::$mConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
 		}
-		return $this->mConfig;
-	}
-
-	/**
-	 * Returns a Google_Service_Plus object from the Google_Client object
-	 * @return Google_Service_Plus
-	 */
-	public function getPlus() {
-		if ( empty( $this->mPlusClient ) ) {
-			$client = $this->mGoogleClient;
-			$this->mPlusClient = new Google_Service_Plus( $client );
-		}
-		return $this->mPlusClient;
-	}
-
-	/**
-	 * Returns a full login URL to Special:GoogleLogin like normal Login link
-	 * @param SkinTemplate $skin The SkinTemplate Object to create link from
-	 * @param Title|null $title Title object of the actual page or null
-	 * @return string
-	 */
-	public function getLoginUrl( SkinTemplate $skin, $title = null ) {
-		$user = $this->getUser();
-		$request = $this->getRequest();
-		if ( $title === null ) {
-			$title = $this->getTitle();
-		}
-		if ( $user->isAllowed( 'read' ) ) {
-			$page = $title;
-		} else {
-			$page = Title::newFromText( $request->getVal( 'title', '' ) );
-		}
-		$page = $request->getVal( 'returnto', $page );
-		$a = [];
-		if ( strval( $page ) !== '' ) {
-			$a['returnto'] = $page;
-			$query = [];
-			if ( !$request->wasPosted() ) {
-				$query = $request->getValues();
-				unset( $query['title'] );
-				unset( $query['returnto'] );
-				unset( $query['returntoquery'] );
-			}
-			$thisQuery = wfArrayToCgi( $query );
-			$query = $request->getVal( 'returntoquery', $thisQuery );
-			if ( $query != '' ) {
-				$a['returntoquery'] = $query;
-			}
-		}
-		$returnto = wfArrayToCgi( $a );
-		return $skin->makeSpecialUrl( 'GoogleLogin', $returnto );
-	}
-
-	/**
-	 * Provide the URL to navigate to the GL special page
-	 * @return string GL special page local url
-	 */
-	public function getSpecialPageUri() {
-		return $this->getSpecialPageTitle()->getLocalUrl();
-	}
-
-	/**
-	 * Returns, if the user want to keep his login (the value of keep login will be deleted!)
-	 * @return boolean
-	 */
-	public function getKeepLogin() {
-		if ( $this->getGLConfig()->get( 'GLForceKeepLogin' ) ) {
-			return true;
-		}
-		$request = $this->getRequest();
-		$status = $request->getSessionData( 'wpGoogleLoginRemember' );
-		$request->setSessionData( 'wpGoogleLoginRemember', null );
-		return ( $status ? true : false );
-	}
-
-	/**
-	 * Generates a request token to send as securehash and save this token as session variable
-	 * @return string
-	 */
-	public function getRequestToken() {
-		$request = $this->getRequest();
-		if ( !isset( $this->mRequestToken ) ) {
-			$this->mRequestToken = md5( rand() );
-		}
-		$request->setSessionData( 'googlelogin_secure_token', $this->mRequestToken );
-		return $this->mRequestToken;
-	}
-
-	/**
-	 * Get recirect location data
-	 * @return array Array with title and query for redirect location
-	 */
-	public function getReturnTo() {
-		$request = $this->getRequest();
-		$returnTo = [];
-		$returnTo['title'] = $request->getSessionData( 'google-returnto' );
-		$query = $request->getSessionData( 'google-returntoquery' );
-		$returnTo['query'] = '';
-		if ( $query ) {
-			$returnTo['query'] = $request->getSessionData( 'google-returntoquery' );
-		}
-		$request->setSessionData( 'google-returnto', null );
-		return $returnTo;
-	}
-
-	/**
-	 * Helps to set the correct values for post login redirect, e.g. keep login
-	 */
-	public function setLoginParameter( $request ) {
-		if ( $request->getVal( 'wpGoogleLoginRemember' ) === "1" ) {
-			$this->setKeepLogin( true );
-		}
-		$returnTo = $request->getVal( 'returnto' );
-		if ( $returnTo !== null ) {
-			$this->setReturnTo( $returnTo, $request->getVal( 'returntoquery' ) );
-		}
-	}
-
-	/**
-	 * Set redirect location, if provided
-	 * @param string $returnTo Location to redirect after Login
-	 * @param string $returnToQuery Query for the Location URL
-	 */
-	public function setReturnTo( $returnTo, $returnToQuery ) {
-		$request = $this->getRequest();
-		$request->setSessionData( 'google-returnto', $returnTo );
-		if ( isset( $returnToQuery ) ) {
-			$request->setSessionData( 'google-returntoquery', $returnToQuery );
-		}
-	}
-
-	/**
-	 * Set the value, if the user want to keep his login
-	 * @param boolean $status True, if the user wants to keep
-	 */
-	public function setKeepLogin( $status = false ) {
-		$request = $this->getRequest();
-		$request->setSessionData( 'wpGoogleLoginRemember', $status );
-	}
-
-	/**
-	 * Checks if the user is allowed to create a new account with Google Login.
-	 */
-	public function isCreateAllowed() {
-		$glConfig = $this->getGLConfig();
-		return $glConfig->get( 'GLAllowAccountCreation' );
-	}
-
-	/**
-	 * Checks, if the submitted secure token is valid (check before do any
-	 * "write" action after form submit)
-	 * @return boolean
-	 */
-	public function isValidRequest() {
-		$request = $this->getRequest();
-		if (
-			( $request->getSessionData( 'googlelogin_secure_token' ) ===
-				$request->getVal( 'wpSecureHash' ) ) &&
-			( $request->getSessionData( 'googlelogin_secure_token' ) !== null &&
-				$request->getVal( 'wpSecureHash' ) !== null )
-		) {
-			$request->setSessionData( 'googlelogin_secure_token', '' );
-			return true;
-		} else {
-			return false;
-		}
+		return self::$mConfig;
 	}
 
 	/**
@@ -213,12 +50,12 @@ class GoogleLogin extends ContextSource {
 	 * @param string $mailDomain The domain of email address
 	 * @return boolean
 	 */
-	public function isValidDomain( $mailDomain ) {
-		$glConfig = $this->getGLConfig();
+	public static function isValidDomain( $mailDomain ) {
+		$glConfig = self::getGLConfig();
 		if ( is_array( $glConfig->get( 'GLAllowedDomains' ) ) ) {
 			if (
 				in_array(
-					$this->getHost( $mailDomain ),
+					self::getHost( $mailDomain ),
 					$glConfig->get( 'GLAllowedDomains' )
 				)
 			) {
@@ -230,99 +67,22 @@ class GoogleLogin extends ContextSource {
 	}
 
 	/**
-	 * Checks, if the Username is valid and createable
-	 * @param string $name The username to check
-	 * @return boolean
-	 */
-	public static function isValidUserName( $name ) {
-		$user = MWUser::newFromName( $name, 'creatable' );
-		if ( $user && $user->getId() === 0 ) {
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Checks if a User in the given user object exists or not.
-	 * @param User $user the User object to check
-	 * @return boolean
-	 */
-	private function userExist( MWUser $user ) {
-		$userName = $user->getName();
-		$checkUser = MWUser::newFromName( $userName );
-		if ( $checkUser ? $checkUser->getId() !== 0 : false ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	 * Handles the login for an authenticated Google User and redirect to the Main page
-	 * @param UserId $id The User id to login
-	 * @param Google-ID $googleId Id f the Googleuser
-	 * @return Status Object of Status with the status of login (and value of redirectto if Ok)
-	 * otherwise (in case of Fatal) an error message.
-	 */
-	public function loginGoogleUser( $id, $googleId ) {
-		$status = new Status;
-		$out = $this->getOutput();
-		$user = MWUser::newFromId( $id );
-		if ( !$this->userExist( $user ) ) {
-			$status = Status::newFatal( 'googlelogin-error-unknownconnected' );
-			$gluser = User::newFromGoogleId( $googleId );
-			$gluser->terminateGoogleConnection();
-			return $status;
-		}
-		if ( $user->isBlocked() ) {
-			$status = Status::newFatal( 'login-userblocked' );
-			return $status;
-		}
-		$user->setCookies( null, null, $this->getKeepLogin() );
-		if ( $user->isLoggedIn() ) {
-			$returnTo = $this->getReturnTo();
-			$title = Title::newFromText( $returnTo['title'] );
-			if ( empty( $returnTo ) || !$title ) {
-				$returnTo = Title::newMainPage()->getFullURL();
-			} else {
-				$returnTo = $title->getFullURL( $returnTo['query'] );
-			}
-			$status = Status::newGood( $returnTo );
-		} else {
-			$status = Status::newFatal( 'googlelogin-generic-error', 'Unknown' );
-		}
-
-		return $status;
-	}
-
-	/**
-	 * Returns the title object of the GL special page.
-	 * @return Title
-	 */
-	private function getSpecialPageTitle() {
-		if ( empty( $this->mSpecialPageTitle ) ) {
-			$this->mSpecialPageTitle = SpecialPage::getTitleFor( 'GoogleLogin' );
-		}
-		return $this->mSpecialPageTitle;
-	}
-
-	/**
 	 * Returns the domain and tld (without subdomains) of the provided E-Mailadress
 	 * @param string $domain The domain part of the email address to extract from.
 	 * @return string The Tld and domain of $domain without subdomains
 	 * @see http://www.programmierer-forum.de/domainnamen-ermitteln-t244185.htm
 	 */
-	public function getHost( $domain = '' ) {
-		$glConfig = $this->getGLConfig();
-		if ( !empty( $this->mHost ) ) {
-			return $this->mHost;
+	public static function getHost( $domain = '' ) {
+		$glConfig = self::getGLConfig();
+		if ( !empty( self::$mHost ) ) {
+			return self::$mHost;
 		}
 		$dir = __DIR__ . "/..";
 		if ( $glConfig->get( 'GLAllowedDomainsStrict' ) ) {
 			$domain = explode( '@', $domain );
 			// we can trust google to give us only valid email address, so give the last element
-			$this->mHost = array_pop( $domain );
-			return $this->mHost;
+			self::$mHost = array_pop( $domain );
+			return self::$mHost;
 		}
 		// for parse_url()
 		$domain =
@@ -346,7 +106,7 @@ class GoogleLogin extends ContextSource {
 
 		// tld extract
 		if ( !file_exists( "$dir/cache/tld.txt" ) ) {
-			$this->createTLDCache( "$dir/cache/tld.txt" );
+			self::createTLDCache( "$dir/cache/tld.txt" );
 		}
 		require ( "$dir/cache/tld.txt" );
 		$tlds = array_flip( $tlds );
@@ -379,8 +139,8 @@ class GoogleLogin extends ContextSource {
 		} else { // first level is TLD
 			$domain = $_2nd;
 		}
-		$this->mHost = $domain;
-		return $this->mHost;
+		self::$mHost = $domain;
+		return self::$mHost;
 	}
 
 	/**
@@ -391,7 +151,7 @@ class GoogleLogin extends ContextSource {
 	 * example.lib.wy.us (3)?)
 	 * @see http://www.programmierer-forum.de/domainnamen-ermitteln-t244185.htm
 	 */
-	private function createTLDCache( $cacheFile, $max_tl = 2 ) {
+	public static function createTLDCache( $cacheFile, $max_tl = 2 ) {
 		$cacheFolder = str_replace( basename( $cacheFile ), '', $cacheFile );
 		if ( !is_writable( $cacheFolder ) ) {
 			throw new MWException( $cacheFolder . ' is not writeable!' );
@@ -431,180 +191,5 @@ class GoogleLogin extends ContextSource {
 				var_export( $tlds, true )
 			) . ";\n?" . ">"
 		);
-	}
-
-	/**
-	 * Sets some standard parameters to the Google Client provided, so the client is able to request
-	 * the data from Google.
-	 * @param Google_Client $client The Google_Client Object to prepare for
-	 * @return Google_Client A prepared instance of Google Client class
-	 */
-	private function prepareClient( $client, $redirectURI ) {
-		$glConfig = $this->getGLConfig();
-		$client->setClientId( $glConfig->get( 'GLAppId' ) );
-		$client->setClientSecret( $glConfig->get( 'GLSecret' ) );
-		$client->setRedirectUri( WebRequest::detectServer().$redirectURI );
-		$client->addScope( 'profile' );
-		$client->addScope( 'email' );
-		return $client;
-	}
-
-	/**
-	 * Includes and initiates the needed Google API Files and classes.
-	 * @return Google_Client
-	 * @throw MWException if required files does not exist
-	 */
-	private function includeAPIFiles() {
-		$dir = __DIR__;
-		if ( class_exists( 'Google_Client' ) ) {
-			$client = new Google_Client();
-			return $client;
-		} else {
-			throw new MWException(
-				'GoogleLogin requires the PHP API Client, which you
-					can install with the Extension:GoogleAPIClient or by running composer update.'
-			);
-		}
-	}
-
-	/**
-	 * Callback-function for create form to check, if needed data is set.
-	 * @see SpecialGoogleLogin::createGoogleUserForm()
-	 * @param array $data The form data submitted to check
-	 * @return string|boolean Returns a string with error message, when check fails, and boolean
-	 *	for success.
-	 */
-	public static function submitChooseName( $data ) {
-		if (
-			!isset( $data['ChooseName'] ) ||
-			empty( $data['ChooseName'] )
-		) {
-			return wfMessage( 'googlelogin-form-choosename-error' )->text();
-		} elseif ( $data['ChooseOwn'] === 'wpAlreadyRegistered' ) {
-			return true;
-		} else {
-			if ( $data['ChooseName'] == 'wpOwn' && empty( $data['ChooseOwn'] ) ) {
-				return wfMessage( 'googlelogin-form-chooseown-error' )->text();
-			}
-			if (
-				$data['ChooseName'] == 'wpOwn' &&
-				!empty( $data['ChooseOwn'] ) &&
-				!GoogleLogin::isValidUserName( $data['ChooseOwn'] )
-			) {
-				return wfMessage( 'googlelogin-form-choosename-existerror', $data['ChooseOwn'] );
-			}
-		}
-		return true;
-	}
-
-	/**
-	 * Generic callback button (always returns true)
-	 * @param array $data The form data submitted to check
-	 * @return string|boolean Returns a string with error message, when check fails, and boolean
-	 *	for success.
-	 */
-	public static function submitGeneric( $data ) {
-		return true;
-	}
-
-	/**
-	 * Handles all catchable fatal errors
-	 *
-	 * @param integer $errorNo error Level
-	 * @param string $errorString error message
-	 * @param string $errorFile in which file the error raised
-	 * @param integer $errorLine the line in $errorFile the error raised in
-	 * @return boolean Always true
-	 */
-	public static function catchableFatalHandler(
-		$errorNo, $errorString, $errorFile, $errorLine
-	) {
-		global $wgOut;
-		$wgOut->addWikiMsg( 'googlelogin-generic-error', $errorString );
-		return true;
-	}
-
-	public static function getLoginCreateForm( &$tpl, $login = true ) {
-		$glConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
-		$request = $tpl->getSkin()->getRequest();
-		$out = $tpl->getSkin()->getOutput();
-		// actual styling doesn't work on create account page, there's already a nice div using the space
-		$showRight = $login ? $glConfig->get( 'GLShowRight' ) : false;
-
-		// disallow login with google, if the user has to login to link the
-		// google account with the wiki account
-		if ( $request->getVal( 'loginmerge' ) === '1' ) {
-			return;
-		}
-		// add default css module
-		$modules = [ 'ext.GoogleLogin.style' ];
-		// if the administrator requested to show the form at the right side,
-		// add this module, too
-		if ( $showRight ) {
-			$modules[] = 'ext.GoogleLogin.right.style';
-		}
-		$out->addModuleStyles( $modules );
-
-		// keep login is only added, if enabled in the configuration
-		$keepLogin = [];
-		if ( $glConfig->get( 'GLShowKeepLogin' ) && $login ) {
-			$keepLogin = [
-				'wpGoogleLoginRemember' => [
-					'type' => 'check',
-					'value' => '1',
-					'id' => 'wpGoogleLoginRemember',
-					'name' => 'wpGoogleLoginRemember',
-					'label' => $tpl->getMsg( 'userlogin-remembermypassword' )->escaped()
-				]
-			];
-		}
-
-		// get the correct button value
-		if ( $login ) {
-			// login form value
-			$buttonMsg = $tpl->getMsg( 'googlelogin' )->escaped();
-		} else {
-			// create form value
-			$buttonMsg = $tpl->getMsg( 'googlelogin-create' )->escaped();
-		}
-
-		// google login form button
-		$fields = [
-			'googleLoginButton' => [
-				'type' => 'submit',
-				'name' => 'googlelogin-submit',
-				'default' => $buttonMsg,
-				'flags' => [ 'primary', 'destructive' ],
-			]
-		] + $keepLogin;
-
-		$hiddenFields = [
-			'returnto' => $request->getVal( 'returnto' ),
-			'returntoquery' => $request->getVal( 'returntoquery' ),
-		];
-
-		// create a separate vform above the "normal" user login form
-		// this could be converted to ooui, after the core login form uses ooui elements and styling
-		$htmlForm = HTMLForm::factory( 'vform', $fields, $tpl->getSkin()->getContext() );
-
-		$htmlForm
-			->suppressDefaultSubmit()
-			->setId( 'googleloginForm' )
-			->setTitle( SpecialPage::getTitleFor( 'GoogleLogin' ) )
-			->addHiddenFields( $hiddenFields )
-			->prepareForm()
-			->displayForm( false );
-
-		// if the form is above the login form, show a separator
-		if ( !$showRight ) {
-			$out->addHtml(
-				Html::openElement( 'fieldset', [ 'class' => 'loginSeperator mw-ui-vform' ] ) .
-				Html::element( 'legend', [], $tpl->getMsg( 'googlelogin-or' )->escaped() ) .
-				Html::closeElement( 'fieldset' )
-			);
-		}
-
-		// allow other extensions to add fields, too (e.g. other buttons)
-		Hooks::run( 'GoogleLoginAfterForm', [ &$tpl ] );
 	}
 }
