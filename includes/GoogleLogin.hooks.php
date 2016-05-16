e46494ec057ca865841e28f99969b6b0e0f0fe80
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-05-17 00:58:02 +0200

    WIP: Rewrite GoogleLogin to be a PrimaryAuthenticationProvider
    This change implements:
     * A new primarey authentication provider (GooglePrimaryAuthenticationProvider),
       which let the user authenticate with the Google account.
     * A workaround for the requirement of a static redirect uri of Google
       (including an own CSRF protection using Google's state field) using a new
       Special page (SPecial:GoogleLoginReturn) which handles the required
       checks for all data and redirects to Special:UserLogin with the correct
       original csrf token of the user login special page.
     * Reworks Special:GoogleLogin to be an information special page only (with
       links to link and unlink)
     * Reworked GoogleUser as a static class (which also doesn't inherit from User
       anymore) for:
       * Get an User object from a Google ID
       * Get a Google ID for a User object
       * Connect an User object with a Google ID
       * Terminate a connection between a Google ID and an User
       * Check, if a Google ID is already connected or not
     * Reworked GoogleLogin.body.php (GoogleLogin class) to remove unnecessary functions,
       and work with new GoogleUser object.
     * Cleaned up GoogleLogin.hooks.php to remove unnecessary hooks
     * Cleaned up extension.json to remove definition for unused hooks
     * Put any class (except Api) into GoogleLogin namespaces
     * Reworks how Google account connections are handled. Now the user can have
       as much Google accounts connected with the wiki account as he wants. This
       changes the 1:1 relationship (used until yet) to a 1:n relationship.
     * Added a SQL patch to remove the primary key from user_googleid in the
       user_google_user table (needed for multiple Google account connections)
    
    This currently breaks the login with the so far used special page GoogleLogin,
    see the todos.
    
    Bug: T110294
    Change-Id: I0d0447eb35ff38fab8bd4d945469cd66cabe0f73

--- includes/GoogleLogin.hooks.php
+++ includes/GoogleLogin.hooks.php
@@ -1,12 +1,20 @@
 <?php
-use GoogleLogin\GoogleUser as User;
+
+namespace GoogleLogin;
+
+use ConfigFactory;
+use Linker;
+use SpecialPage;
+use ChangeTags;
+
+use GoogleLogin\Specials\SpecialGoogleLogin;
 
 class GoogleLoginHooks {
 	public static function onUserLogoutComplete() {
-		$googleLogin = new GoogleLogin;
-		$request = $googleLogin->getRequest();
-		if ( $request->getSessionData( 'access_token' ) !== null ) {
-			$request->setSessionData( 'access_token', null );
+		global $wgRequest;
+
+		if ( $wgRequest->getSessionData( 'access_token' ) !== null ) {
+			$wgRequest->setSessionData( 'access_token', null );
 		}
 	}
 
@@ -20,71 +28,17 @@ class GoogleLoginHooks {
 		) {
 			return true;
 		}
+
 		// Tables to add to the database
 		$tables = [ 'user_google_user' ];
 		// Sql directory inside the extension folder
 		$sql = __DIR__ . '/sql';
-		// Extension of the table schema file (depending on the database type)
-		switch ( $updater !== null ? $updater->getDB()->getType() : $config->get( 'DBtype' ) ) {
-			default:
-				$ext = 'sql';
-		}
-		// Do the updating
-		foreach ( $tables as $table ) {
-			// Location of the table schema file
-			$schema = "$sql/$table.$ext";
-			$updater->addExtensionUpdate( [ 'addTable', $table, $schema, true ] );
-		}
+		$schema = "$sql/user_google_user.sql";
+		$updater->addExtensionUpdate( [ 'addTable', 'user_google_user', $schema, true ] );
+		$updater->modifyExtensionField( 'user_google_user',
+			'user_googleid',
+			"$sql/user_google_id_no_primary.sql" );
 		return true;
-	}
-
-	public static function onUserLoginForm( &$tpl ) {
-		GoogleLogin::getLoginCreateForm( $tpl );
-	}
-
-	public static function onUserCreateForm( &$tpl ) {
-		GoogleLogin::getLoginCreateForm( $tpl, false );
-	}
-
-	/**
-	 * Handles the replace of Loginlink and deletion of Create account link in personal tools
-	 * if Loginreplacement is configured.
-	 */
-	public static function onPersonalUrls( array &$personal_urls, Title $title, SkinTemplate $skin ) {
-		$glConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
-		if ( $glConfig->get( 'GLReplaceMWLogin' ) && array_key_exists( 'login', $personal_urls ) ) {
-			// unset the create account link
-			if ( array_key_exists( 'createaccount', $personal_urls ) ) {
-				unset( $personal_urls['createaccount'] );
-			}
-
-			// Replace login link with GoogleLogin link
-			$googleLogin = new GoogleLogin;
-			$personal_urls['login']['text'] = $skin->msg( 'googlelogin' )->text();
-			$personal_urls['login']['href'] = $googleLogin->getLoginUrl( $skin, $title );
-		}
-	}
-
-	/**
-	 * Handles the replace of Special:UserLogin with Special:GoogleLogin if Loginreplacement is
-	 * configured.
-	 */
-	public static function onSpecialPage_initList( &$list ) {
-		// FIXME: Find a better way to access the User object!
-		global $wgUser;
-
-		$glConfig = ConfigFactory::getDefaultInstance()->makeConfig( 'googlelogin' );
-		// Replaces the UserLogin special page if configured and user isn't logged in
-		// TODO: The check for the MW_NO_SESSION constant is an ugly workaround for T135445
-		// given, that the replacement of the user login special page isn't needed after GoogleLogin
-		// was converted to to AuthManager (and the own Special page isn't needed anymore). Task T110294
-		if (
-			!defined( 'MW_NO_SESSION' ) &&
-			!$wgUser->isLoggedIn() &&
-			$glConfig->get( 'GLReplaceMWLogin' )
-		) {
-			$list['Userlogin'] = 'SpecialGoogleLogin';
-		}
 	}
 
 	/**
@@ -96,8 +50,7 @@ class GoogleLoginHooks {
 	*/
 	static function onGetPreferences( $user, &$preferences ) {
 		// check if the userid is linked with a google id
-		$googleUser = User::newFromId( $user->getId() );
-		$userIdExists = $googleUser->hasConnectedGoogleAccount();
+		$userIdExists = GoogleUser::hasConnectedGoogleAccount( $user );
 
 		// generate the content for Special:Preferences
 		$status = ( $userIdExists ? wfMessage( 'googlelogin-linked' )->text() :
@@ -132,43 +85,6 @@ class GoogleLoginHooks {
 	}
 
 	/**
-	 * Replaces the RC comment with a filterable RC tag
-	 *
-	 * @param RecentChange $recentChange The recentChange object
-	 */
-	public static function onRecentChange_save( $recentChange ) {
-		$performer = $recentChange->getPerformer();
-		$attribs = $recentChange->getAttributes();
-
-		if (
-			$performer->getName() === SpecialGoogleLogin::$performer &&
-			$attribs['rc_log_action'] === 'create'
-		) {
-			ChangeTags::addTags( 'googlelogin', $attribs['rc_id'] );
-		}
-	}
-
-	/**
-	 * Register, and mark as active, the 'googlelogin' change tag
-	 *
-	 * @param array $tags
-	 * @return bool
-	 */
-	public static function onListDefinedAndActiveTags( array &$tags ) {
-		$tags[] = 'googlelogin';
-		return true;
-	}
-
-	/**
-	 * Adds a custom valid error message to the login page, used when the user want
-	 * to link an account with the google account and isn't logged in so far.
-	 * @param array $messages Already added messages
-	 */
-	public static function onLoginFormValidErrorMessages( array &$messages ) {
-		$messages[] = 'googlelogin-login-merge-warning';
-	}
-
-	/**
 	 * UnitTestsList hook handler
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/UnitTestsList
 	 *
@@ -186,9 +102,7 @@ class GoogleLoginHooks {
 	 *
 	 * @param array &$updateFields
 	 */
-	public static function onMergeAccountFromTo( &$fromUser, &$toUser ) {
-		$oldUser = \GoogleLogin\GoogleUser::newFromId( $fromUser->getId() );
-		$newUser = \GoogleLogin\GoogleUser::newFromId( $toUser->getId() );
+	public static function onMergeAccountFromTo( &$oldUser, &$newUser ) {
 		// check, if
 		if (
 			// the new user exists (e.g. is not Anonymous)
@@ -199,11 +113,13 @@ class GoogleLoginHooks {
 			$oldUser->hasConnectedGoogleAccount()
 		) {
 			// save the google id of the old account
-			$googleId = $oldUser->getGoogleId();
-			// delete the connection between the google and the old wiki account
-			$oldUser->terminateGoogleConnection();
-			// add the google id to the new account
-			$newUser->connectWithGoogle( $googleId );
+			$googleIds = GoogleUser::getGoogleIdFromUser( $oldUser );
+			foreach( $googleIds as $i => $id ) {
+				// delete the connection between the google and the old wiki account
+				GoogleUser::terminateGoogleConnection( $oldUser, $id );
+				// add the google id to the new account
+				GoogleUser::connectWithGoogle( $newUser, $id );
+			}
 		}
 
 		return true;
---
+++ includes/auth/GoogleServerAuthenticationRequest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * GoogleServerAuthenticationRequest implementation
+ */
+
+namespace GoogleLogin\Auth;
+
+use MediaWiki\Auth\AuthenticationRequest;
+
+/**
+ * Implements a GoogleServerAuthenticationRequest that holds the data returned by a
+ * redirect from Google into the authentication workflow.
+ */
+class GoogleServerAuthenticationRequest extends AuthenticationRequest {
+	/**
+	 * Verification code provided by the server. Needs to be sent back in the last leg of the
+	 * authorization process.
+	 * @var string
+	 */
+	public $accessToken;
+
+	/**
+	 * An error code returned in case of Authentication failure
+	 * @var string
+	 */
+	public $errorCode;
+
+	public function getFieldInfo() {
+		return array(
+			'error' => array(
+				'type' => 'string',
+				'label' => wfMessage( 'googlelogin-param-error-label' ),
+				'help' => wfMessage( 'googlelogin-param-error-help' ),
+			),
+			'code' => array(
+				'type' => 'string',
+				'label' => wfMessage( 'googlelogin-param-code-label' ),
+				'help' => wfMessage( 'googlelogin-param-code-help' ),
+			),
+		);
+	}
+
+	/**
+	 * Load data from query parameters in an OAuth return URL
+	 * @param array $data Submitted data as an associative array
+	 * @return AuthenticationRequest|null
+	 */
+	public function loadFromSubmission( array $data ) {
+		if ( isset( $data['code'] ) ) {
+			$this->accessToken = $data['code'];
+			return true;
+		}
+
+		if ( isset( $data['error'] ) ) {
+			$this->errorCode = $data['error'];
+			return true;
+		}
+		return false;
+	}
+}
