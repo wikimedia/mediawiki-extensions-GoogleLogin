1dbe90a151c9bb17bacb8bfb50f3f762d1d8141e
Florian <florian.schmidt.stargatewissen@gmail.com>
2016-05-17 00:58:02 +0200

    WIP: Rewrite GoogleLogin to be a PrimaryAuthenticationProvider
    This change implements:
     * A new primarey authentication provider (GooglePrimaryAuthenticationProvider),
       which let the user authenticate with the Google account.
     * A workaround for the requirement of a static redirect uri of Google
       (including an own CSRF protection using Google's state field) using a new
       Special page (SPecial:GoogleLoginReturn) which handles the required
       checks for all data and redirects to Special:UserLogin with the correct
       original csrf token of the user login special page.
     * Reworks Special:GoogleLogin to be an information special page only (with
       links to link and unlink)
     * Reworked GoogleUser as a static class (which also doesn't inherit from User
       anymore) for:
       * Get an User object from a Google ID
       * Get a Google ID for a User object
       * Connect an User object with a Google ID
       * Terminate a connection between a Google ID and an User
       * Check, if a Google ID is already connected or not
     * Reworked GoogleLogin.body.php (GoogleLogin class) to remove unnecessary functions,
       and work with new GoogleUser object.
     * Cleaned up GoogleLogin.hooks.php to remove unnecessary hooks
     * Cleaned up extension.json to remove definition for unused hooks
     * Put any class (except Api) into GoogleLogin namespaces
     * Reworks how Google account connections are handled. Now the user can have
       as much Google accounts connected with the wiki account as he wants. This
       changes the 1:1 relationship (used until yet) to a 1:n relationship.
     * Added a SQL patch to remove the primary key from user_googleid in the
       user_google_user table (needed for multiple Google account connections)
    
    This currently breaks the login with the so far used special page GoogleLogin,
    see the todos.
    
    Bug: T110294
    Change-Id: I0d0447eb35ff38fab8bd4d945469cd66cabe0f73

---
+++ includes/auth/GoogleRemoveAuthenticationRequest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * GoogleRemoveAuthenticationRequest implementation
+ */
+
+namespace GoogleLogin\Auth;
+
+use MediaWiki\Auth\AuthenticationRequest;
+use MediaWiki\Auth\AuthManager;
+
+/**
+ * Implementation of an AuthenticationReuqest that is used to remove a
+ * connection between a Google account and a local wiki account.
+ */
+class GoogleRemoveAuthenticationRequest extends AuthenticationRequest {
+	private $googleId = null;
+
+	public function __construct( $googleId ) {
+		$this->googleId = $googleId;
+	}
+
+	public function getUniqueId() {
+		return parent::getUniqueId() . ':' . $this->googleId;
+	}
+
+	public function getFieldInfo() {
+		if ( $this->action === AuthManager::ACTION_REMOVE ) {
+			return [];
+		}
+		return parent::getFieldInfo();
+	}
+
+	/**
+	 * Returns the Google ID, that should be removed from the valid
+	 * credentials of the user.
+	 *
+	 * @return String
+	 */
+	public function getGoogleId() {
+		return $this->googleId;
+	}
+
+	public function describeCredentials() {
+		return [
+			'provider' => wfMessage( 'googlelogin-auth-service-name' ),
+			'account' =>
+				new \RawMessage( '$1', [ $this->googleId ] ),
+		];
+	}
+}
