6c7c5f4e80bcd11428aafa040c54b9c3d3dc5185
Paul Walko <paulsw.pw@gmail.com>
2017-06-01 13:41:16 -0400

    typo
    Change-Id: I22bce4bf8da9af379efbc3c6790bba5952ec022c

--- includes/GoogleLogin.hooks.php
+++ includes/GoogleLogin.hooks.php
@@ -20,7 +20,7 @@ class GoogleLoginHooks {
 
 	public static function onLoadExtensionSchemaUpdates( \DatabaseUpdater $updater = null ) {
 		// Check db type
-		$sql_db_type = $update->getDB()->getType();
+		$sql_db_type = $updater->getDB()->getType();
 		if ( !in_array ( $sql_db_type, [ 'mysql', 'postgres' ] ) ) {
 			throw new MWException("Unsupported DB: $sql_db_type. Only MySQL & PostgreSQL are supported.");
 		}
