9e4434931c240dd31de36edd1fc879eb83261436
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-06-09 20:19:16 +0200

    Add backend services and functions for allowed domains in the database
    Instead of requiring the users of this extension to manage the list of allowed
    domains, from which the users can login with Google, in an array in the Local-
    Settings.php file, this change implements to store this list in the database
    in a new database table.
    
    To allow different backends for the list of allowed domains, a new interface,
    AllowedDomainsStore, is created, which allows different implementations for
    the puprose of checking, if a specific domain is allowed or not (no matter where
    the list of allowed domains is stored or comes from). There're three implementations,
    which are inspired by the SiteStore implementations. One for the currently
    used global array, one for a list out of the database and one which uses a BagOStuff
    cache and another AllowedDomainsStore as a backend.
    
    There's currently no api or user interface to manage the list of allowed domains,
    therefore it has to be done in the database directly. There will be changes in
    the future to allow to manage the list through an api and/or through a user
    interface. The list, when the database is used, will be cached for up to 1 hour,
    so changes will not take effect immediately at the moment.
    
    Bug: T166423
    Change-Id: I629534b97b0eddbc195c87b9a86d015c2e07a694

---
+++ includes/alloweddomains/DBAllowedDomainsStore.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace GoogleLogin\AllowedDomains;
+
+use Wikimedia\Rdbms\LoadBalancer;
+
+/**
+ * Class DBAllowedDomainsStore
+ *
+ * Implementation of the AllowedDomainsStore interface, which is backed by the database. The
+ * allowed domain list is stored and manageable in the database of MediaWiki. For performance
+ * reasons, you should use the CachedAllowedDomainsStore implementation and wrap an instance of
+ * this class into it.
+ *
+ * @package GoogleLogin\AllowedDomains
+ */
+class DBAllowedDomainsStore implements AllowedDomainsStore {
+	private $dbLoadBalancer;
+	private $allowedDomains;
+
+	public function __construct( LoadBalancer $dbLoadBalancer ) {
+		$this->dbLoadBalancer = $dbLoadBalancer;
+	}
+
+	private function loadDomains() {
+		if ( $this->allowedDomains !== null ) {
+			return;
+		}
+		$dbr = $this->dbLoadBalancer->getConnection( DB_REPLICA );
+
+		$res = $dbr->select(
+			'googlelogin_allowed_domains',
+			[
+				'gl_allowed_domain_id',
+				'gl_allowed_domain',
+			],
+			'',
+			__METHOD__
+		);
+
+		foreach ( $res as $row ) {
+			$this->allowedDomains[$row->gl_allowed_domain_id] = $row->gl_allowed_domain;
+		}
+	}
+
+	public function getAllowedDomains() {
+		$this->loadDomains();
+
+		return $this->allowedDomains;
+	}
+
+	public function contains( EmailDomain $domain ) {
+		$this->loadDomains();
+
+		return in_array( $domain->getHost(), $this->allowedDomains );
+	}
+
+	public function add( EmailDomain $domain ) {
+		if ( $this->contains( $domain ) ) {
+			return -1;
+		}
+		$dbw = $this->dbLoadBalancer->getConnection( DB_MASTER );
+		$dbw->insert(
+			'googlelogin_allowed_domains',
+			[
+				'gl_allowed_domain' => $domain->getHost()
+			],
+			__METHOD__
+		);
+
+		return $dbw->insertId();
+	}
+
+	public function clear() {
+		$dbw = $this->dbLoadBalancer->getConnection( DB_MASTER );
+
+		$ok = $dbw->delete( 'googlelogin_allowed_domains', '*', __METHOD__ );
+		$this->allowedDomains = null;
+
+		return $ok;
+	}
+}
