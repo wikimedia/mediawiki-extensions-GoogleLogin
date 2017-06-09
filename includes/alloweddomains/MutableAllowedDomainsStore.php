209728a3e91326ef9460cd8f80a9e23423797668
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-06-09 20:50:16 +0200

    Add API module to manage the list of domains when using the database
    The new googleloginmanagealloweddomain api module allows an user with the
    managegooglelogindomains permission to add and remove domains from the lits
    of allowed domains, as long as the configured backend allows to be changed
    using the API (e.g. the database backend).
    
    Bug: T166423
    Change-Id: I6c0249f6eab7463ae730799b1e6d71580470e2cd

--- includes/alloweddomains/MutableAllowedDomainsStore.php
+++ includes/alloweddomains/MutableAllowedDomainsStore.php
@@ -14,9 +14,19 @@ interface MutableAllowedDomainsStore {
 	 * Adds the host of the given EmailDomain to the store.
 	 *
 	 * @param EmailDomain $domain
-	 * @return void
+	 * @return integer The ID of the newly added entry (0 if IDs are not used, -1 on failure)
 	 */
 	function add( EmailDomain $domain );
+
+	/**
+	 * Removes the given host of the EmailDomain from the AllowedDomainsStore, if it is present.
+	 * Will return true, when the domain is not present anymore (either not present before or
+	 * successfully deleted), false otheriwse.
+	 *
+	 * @param EmailDomain $domain
+	 * @return boolean
+	 */
+	function remove( EmailDomain $domain );
 
 	/**
 	 * Cleans the store and deletes all entries from it.
