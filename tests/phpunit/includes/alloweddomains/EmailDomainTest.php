edd2a2966b54f91c2152f6f9b04fa172005dd52a
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-07-03 18:53:14 +0200

    Rewrite domain host parsing part of EmailDomain
    This also adds some tests to EmailDomain (before I refactored the
    domain host parsing part).
    
    The domain list (public suffix list) is not downloaded "on-the-fly"
    anymore, but is now required to be downloaded beforehand using the
    new maintenance script. The parsing part is also simplified, credits
    goes to mgutt [1], where I was free to get inspiration.
    
    [1] https://stackoverflow.com/a/9632782/3394281
    
    Change-Id: I006de79ca6217ec7da662a04b5baff000058c98c

---
+++ tests/phpunit/includes/alloweddomains/EmailDomainTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace GoogleLogin\AllowedDomains;
+
+class EmailDomainTest extends \MediaWikiTestCase {
+	private static $preDefinedSuffixes = [
+		'com',
+		'us.co',
+	];
+
+	/**
+	 * @var EmailDomain
+	 */
+	private $googleMail;
+
+	/**
+	 * @var EmailDomain
+	 */
+	private $subdomainMail;
+
+	/**
+	 * @var EmailDomain
+	 */
+	private $emptyMail;
+
+	/**
+	 * @var EmailDomain
+	 */
+	private $twoSuffixMail;
+
+	public function setUp() {
+		parent::setUp();
+		$this->googleMail = new EmailDomain( 'test@gmail.com', false, self::$preDefinedSuffixes );
+		$this->emptyMail = new EmailDomain( '', false, self::$preDefinedSuffixes );
+		$this->subdomainMail =
+			new EmailDomain( 'test@my.subdomain.com', false, self::$preDefinedSuffixes );
+		$this->twoSuffixMail =
+			new EmailDomain( 'test@my.subdomain.co.us', false, self::$preDefinedSuffixes );
+	}
+
+	/**
+	 * @covers \GoogleLogin\AllowedDomains\EmailDomain::getEmail()
+	 */
+	public function testGetEMail() {
+		$this->assertEquals( 'test@gmail.com', $this->googleMail->getEmail() );
+		$this->assertEquals( '', $this->emptyMail->getEmail() );
+		$this->assertEquals( 'test@my.subdomain.com', $this->subdomainMail->getEmail() );
+		$this->assertEquals( 'test@my.subdomain.co.us', $this->twoSuffixMail->getEmail() );
+	}
+
+	/**
+	 * @covers \GoogleLogin\AllowedDomains\EmailDomain::getHost()
+	 */
+	public function testGetHost() {
+		$this->assertEquals( '', $this->emptyMail->getHost() );
+		$this->assertEquals( 'gmail.com', $this->googleMail->getHost() );
+		$this->assertEquals( 'subdomain.com', $this->subdomainMail->getHost() );
+		$this->assertEquals( 'subdomain.co.us', $this->twoSuffixMail->getHost() );
+	}
+
+
+	/**
+	 * @covers \GoogleLogin\AllowedDomains\EmailDomain::getHost()
+	 */
+	public function testGetHostStrict() {
+		$emptyMailStrict = new EmailDomain( '', true, self::$preDefinedSuffixes );
+		$googleMailStrict = new EmailDomain( 'test@gmail.com', true, self::$preDefinedSuffixes );
+		$subdomainMailStrict =
+			new EmailDomain( 'test@my.subdomain.com', true, self::$preDefinedSuffixes );
+		$twoSuffixMailStrict =
+			new EmailDomain( 'test@my.subdomain.co.us', true, self::$preDefinedSuffixes );
+		$this->assertEquals( '', $emptyMailStrict->getHost() );
+		$this->assertEquals( 'gmail.com', $googleMailStrict->getHost() );
+		$this->assertEquals( 'my.subdomain.com', $subdomainMailStrict->getHost() );
+		$this->assertEquals( 'my.subdomain.co.us', $twoSuffixMailStrict->getHost() );
+	}
+}
