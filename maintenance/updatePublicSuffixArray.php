d621316ca8e428a3ca910989b3d308521d96f6de
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-07-03 18:53:14 +0200

    Rewrite domain host parsing part of EmailDomain
    This also adds some tests to EmailDomain (before I refactored the
    domain host parsing part).
    
    The domain list (public suffix list) is not downloaded "on-the-fly"
    anymore, but is now required to be downloaded beforehand using the
    new maintenance script. The parsing part is also simplified, credits
    goes to mgutt [1], where I was free to get inspiration.
    
    [1] https://stackoverflow.com/a/9632782/3394281
    
    Change-Id: I006de79ca6217ec7da662a04b5baff000058c98c

---
+++ maintenance/updatePublicSuffixArray.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Remove invalid events from echo_event and echo_notification
+ *
+ * @ingroup Maintenance
+ */
+
+require_once getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
+	: __DIR__ . '/../../../maintenance/Maintenance.php';
+
+/**
+ * Maintenance script that updates the public suffix array.
+ *
+ * @ingroup Maintenance
+ */
+class UpdatePublicSuffixArray extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+
+		$this->mDescription = 'Updates the list of public suffixes used for domain recognition.';
+		$this->requireExtension( 'GoogleLogin' );
+	}
+
+	public function execute() {
+		$arrayDirectory = __DIR__ . '/../';
+		if ( !is_writable( $arrayDirectory ) ) {
+			throw new MWException( $arrayDirectory . ' is not writeable!' );
+		}
+		$publicSuffixList = file(
+			'https://publicsuffix.org/list/public_suffix_list.dat'
+		);
+		if ( $publicSuffixList === false ) {
+			throw new MWException( 'Domainlist can not be downloaded!' );
+		}
+		$publicSuffixes = [];
+
+		foreach ( $publicSuffixList as $suffix ) {
+			$suffix = trim( $suffix );
+
+			if ( !$suffix || strpos( $suffix, '/' ) === 0 ) {
+				continue;
+			}
+			if ( strpos( $suffix, '*.' ) !== false ) {
+				$suffix = substr( $suffix, 2 );
+			}
+			if ( strpos( $suffix, '!' ) === 0 ) {
+				$suffix = substr( $suffix, 1 );
+			}
+			$suffix = implode( '.', array_reverse(
+				explode(
+					'.',
+					$suffix
+				)
+			) );
+			$publicSuffixes[] = $suffix;
+		}
+
+		file_put_contents(
+			$arrayDirectory . \GoogleLogin\Constants::PUBLIC_SUFFIX_ARRAY_FILE,
+			"<?php\n" . 'return [ "' . implode( "\",\n\"", $publicSuffixes ) . '" ];'
+		);
+	}
+}
+$maintClass = 'UpdatePublicSuffixArray';
+require_once RUN_MAINTENANCE_IF_MAIN;
