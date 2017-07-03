61a28615b77d62644dfb6a4278f7ee6bf2d98cde
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2017-07-03 18:53:14 +0200

    Rewrite domain host parsing part of EmailDomain
    This also adds some tests to EmailDomain (before I refactored the
    domain host parsing part).
    
    The domain list (public suffix list) is not downloaded "on-the-fly"
    anymore, but is now required to be downloaded beforehand using the
    new maintenance script. The parsing part is also simplified, credits
    goes to mgutt [1], where I was free to get inspiration.
    
    [1] https://stackoverflow.com/a/9632782/3394281
    
    Change-Id: I006de79ca6217ec7da662a04b5baff000058c98c

--- includes/Constants.php
+++ includes/Constants.php
@@ -10,4 +10,5 @@ namespace GoogleLogin;
 
 class Constants {
 	const SERVICE_ALLOWED_DOMAINS_STORE = 'GoogleLogin.AllowedDomainsStore';
+	const PUBLIC_SUFFIX_ARRAY_FILE = 'publicSuffixArray.php';
 }
--- includes/alloweddomains/EmailDomain.php
+++ includes/alloweddomains/EmailDomain.php
@@ -2,7 +2,7 @@
 
 namespace GoogleLogin\AllowedDomains;
 
-use MWException;
+use GoogleLogin\Constants;
 
 /**
  * Represents a single E-Mail address.
@@ -11,7 +11,12 @@ use MWException;
  */
 class EmailDomain {
 	private $emailAddress;
-	private $domainHost;
+	private $domainHost = '';
+
+	/**
+	 * @var array
+	 */
+	private $publicSuffixes;
 
 	/**
 	 * EmailDomain constructor.
@@ -21,8 +26,14 @@ class EmailDomain {
 	 *  test@test.example.com will be converted to example.com if this is false)
 	 */
 	public function __construct( $mail, $strict = false ) {
+		$this->publicSuffixes =
+			array_flip( include __DIR__ . '/../../' . Constants::PUBLIC_SUFFIX_ARRAY_FILE );
+
 		$this->emailAddress = $mail;
-		$this->domainHost = $this->parseHost( $mail, $strict );
+		$domain = explode( '@', $mail );
+		if ( isset( $domain[1] ) ) {
+			$this->domainHost = $this->parseHost( $domain[1], $strict );
+		}
 	}
 
 	/**
@@ -46,121 +57,32 @@ class EmailDomain {
 	 * Returns the domain and tld (without subdomains) of the provided E-Mailadress
 	 * @param string $domain The domain part of the email address to extract from.
 	 * @return string The Tld and domain of $domain without subdomains
-	 * @see http://www.programmierer-forum.de/domainnamen-ermitteln-t244185.htm
 	 */
 	private function parseHost( $domain = '', $strict ) {
-		$dir = __DIR__ . "/../..";
 		if ( $strict ) {
-			$domain = explode( '@', $domain );
 			// we can trust google to give us only valid email address, so give the last element
-			return array_pop( $domain );
+			return $domain;
 		}
-		// for parse_url()
-		$domain =
-			!isset( $domain[5] ) ||
-			(
-				$domain[3] != ':' &&
-				$domain[4] != ':' &&
-				$domain[5] != ':'
-			) ? 'http://' . $domain : $domain;
-		// remove "/path/file.html", "/:80", etc.
-		$domain = parse_url( $domain, PHP_URL_HOST );
-		// separate domain level
-		$lvl = explode( '.', $domain ); // 0 => www, 1 => example, 2 => co, 3 => uk
-		// set levels
-		krsort( $lvl ); // 3 => uk, 2 => co, 1 => example, 0 => www
-		$lvl = array_values( $lvl ); // 0 => uk, 1 => co, 2 => example, 3 => www
-		$_1st = $lvl[0];
-		$_2nd = isset( $lvl[1] ) ? $lvl[1] . '.' . $_1st : false;
-		$_3rd = isset( $lvl[2] ) ? $lvl[2] . '.' . $_2nd : false;
-		$_4th = isset( $lvl[3] ) ? $lvl[3] . '.' . $_3rd : false;
 
-		// tld extract
-		if ( !file_exists( "$dir/cache/tld.txt" ) ) {
-			$this->createTLDCache( "$dir/cache/tld.txt" );
-		}
-		require "$dir/cache/tld.txt";
-		$tlds = array_flip( $tlds );
-		if ( // fourth level is TLD
-			$_4th &&
-			!isset( $tlds[ '!' . $_4th ] ) &&
-			(
-				isset( $tlds[ $_4th ] ) ||
-				isset( $tlds[ '*.' . $_3rd ] )
-			)
-		) {
-			$domain = isset( $lvl[4] ) ? $lvl[4] . '.' . $_4th : false;
-		} elseif ( // third level is TLD
-			$_3rd &&
-			!isset( $tlds[ '!' . $_3rd ] ) &&
-			(
-				isset( $tlds[ $_3rd ] ) ||
-				isset( $tlds[ '*.' . $_2nd ] )
-			)
-		) {
-			$domain = $_4th;
-		} elseif ( // second level is TLD
-			!isset( $tlds[ '!' . $_2nd ] ) &&
-			(
-				isset( $tlds[ $_2nd ] ) ||
-				isset( $tlds[ '*.' . $_1st ] )
-			)
-		) {
-			$domain = $_3rd;
-		} else { // first level is TLD
-			$domain = $_2nd;
-		}
-		return $domain;
+		$url = explode( '.', $domain );
+
+		return $this->getDomainPart( $url );
 	}
 
 	/**
-	 * Creates the TLD cache from which the valid tld of mail domain comes from.
-	 * @param string $cacheFile The file to create the cache too (must be writeable for the
-	 * webserver!)
-	 * @param int $max_tl How deep the domain list is (enclude example.co.uk (2) or
-	 * example.lib.wy.us (3)?)
-	 * @see http://www.programmierer-forum.de/domainnamen-ermitteln-t244185.htm
-	 * @throws MWException
+	 * @param $url
+	 * @return string
 	 */
-	private function createTLDCache( $cacheFile, $max_tl = 2 ) {
-		$cacheFolder = str_replace( basename( $cacheFile ), '', $cacheFile );
-		if ( !is_writable( $cacheFolder ) ) {
-			throw new MWException( $cacheFolder . ' is not writeable!' );
-		}
-		$tlds = file(
-			'http://mxr.mozilla.org/mozilla-central/source/netwerk/dns/effective_tld_names.dat?raw=1'
-		);
-		if ( $tlds === false ) {
-			throw new MWException( 'Domainlist can not be downloaded!' );
-		}
-		$i = 0;
-		// remove unnecessary lines
-		foreach ( $tlds as $tld ) {
-			$tlds[ $i ] = trim( $tld );
-			/**
-			 *	empty
-			 *	comments
-			 *	top level domains
-			 *	is overboard
-			 */
-			if (
-				!$tlds[ $i ] ||
-				$tld[0] == '/' ||
-				strpos( $tld, '.' ) === false ||
-				substr_count( $tld, '.' ) >= $max_tl
-			) {
-				unset( $tlds[ $i ] );
+	private function getDomainPart( $url ) {
+		$parts = array_reverse( $url );
+		foreach ( $parts as $key => $part ) {
+			$tld = implode( '.', $parts );
+			if ( isset( $this->publicSuffixes[$tld] ) ) {
+				return implode( '.', array_slice( $url, $key - 1 ) );
 			}
-			$i++;
+			array_pop( $parts );
 		}
-		$tlds = array_values( $tlds );
-		file_put_contents(
-			$cacheFile,
-			"<?php\n" . '$tlds = ' . str_replace(
-				[ ' ', "\n" ],
-				'',
-				var_export( $tlds, true )
-			) . ";\n?" . ">"
-		);
+
+		return implode( '.', $url );
 	}
 }
