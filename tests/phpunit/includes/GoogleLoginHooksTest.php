9a4cb1898ecd24e39566ea6ef3d192423a4241aa
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2018-06-06 19:09:26 +0200

    Do not conditionally add database tables to the database
    Bug: T196173
    Change-Id: I8df342c421ff0f373ed1446e55d403ab81c3a671

--- tests/phpunit/includes/GoogleLoginHooksTest.php
+++ tests/phpunit/includes/GoogleLoginHooksTest.php
@@ -18,6 +18,9 @@ class GoogleLoginHooksTest extends \MediaWikiTestCase {
 	 * @covers \GoogleLogin\GoogleLoginHooks::onApiMainModuleManager()
 	 */
 	public function testOnApiMainModuleManagerDefault() {
+		$this->setMwGlobals( [
+			'wgGLAllowedDomainsDB' => false,
+		] );
 		GoogleLoginHooks::onApiMainModuleManager( $this->moduleManager );
 		$this->assertFalse( $this->moduleManager->isDefined( 'googleloginmanagealloweddomain' ) );
 	}
@@ -59,5 +62,54 @@ class GoogleLoginHooksTest extends \MediaWikiTestCase {
 		] );
 		GoogleLoginHooks::onApiMainModuleManager( $this->moduleManager );
 		$this->assertTrue( $this->moduleManager->isDefined( 'googleloginmanagealloweddomain' ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\GoogleLoginHooks::onLoadExtensionSchemaUpdates()
+	 */
+	public function testOnLoadExtensionSchemaUpdatesAddsToShared() {
+		$this->setMwGlobals( [
+			'wgSharedDB' => true,
+		] );
+		$dbUpdaterMock = $this
+			->getMockBuilder( \DatabaseUpdater::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$dbUpdaterMock->method( 'getDB' )->willReturn( wfGetDB( DB_REPLICA ) );
+		$dbUpdaterMock
+			->expects( $this->atLeastOnce() )
+			->method( 'addExtensionUpdate' );
+
+		GoogleLoginHooks::onLoadExtensionSchemaUpdates( $dbUpdaterMock );
+	}
+
+	/**
+	 * @covers \GoogleLogin\GoogleLoginHooks::onLoadExtensionSchemaUpdates()
+	 * @dataProvider provideAllowedDomainsConfig
+	 */
+	public function testOnLoadExtensionSchemaUpdatesAddsAllowdDomains( $isAllowedDomainsEnabled ) {
+		$this->setMwGlobals( [
+			'wgGLAllowedDomainsDB' => $isAllowedDomainsEnabled,
+		] );
+		$dbUpdaterMock = $this
+			->getMockBuilder( \DatabaseUpdater::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$dbUpdaterMock->method( 'getDB' )->willReturn( wfGetDB( DB_REPLICA ) );
+		$dbUpdaterMock
+			->expects( $this->exactly( 2 ) )
+			->method( 'addExtensionUpdate' )
+			->with( $this->callback( function ( $arg ) {
+				return in_array( $arg[1], [ 'user_google_user', 'googlelogin_allowed_domains' ] );
+			} ) );
+
+		$this->assertTrue( GoogleLoginHooks::onLoadExtensionSchemaUpdates( $dbUpdaterMock ) );
+	}
+
+	public function provideAllowedDomainsConfig() {
+		return [
+			[ true ],
+			[ false ],
+		];
 	}
 }
