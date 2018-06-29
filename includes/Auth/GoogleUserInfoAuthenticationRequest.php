9cfa2b0c9a8fc6af4d81816f8755b28a5bc2d562
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2018-06-29 02:59:28 +0200

    Remove profile permission scope from requested account permissions
    Instead, use the id_token information from the email scope only for
    authenticating or creating a new account with MediaWiki.
    
    Bug: T198439
    Change-Id: I59ec4842ea54bdc4bfd487df3b5092e66d898f01

--- includes/Auth/GoogleUserInfoAuthenticationRequest.php
+++ includes/Auth/GoogleUserInfoAuthenticationRequest.php
@@ -9,11 +9,11 @@ use GoogleLogin\GoogleUser;
 use MediaWiki\Auth\AuthenticationRequest;
 
 /**
- * An AUthenticationRequest that holds Google user information.
+ * An AuthenticationRequest that holds Google user information.
  */
 class GoogleUserInfoAuthenticationRequest extends AuthenticationRequest {
 	public $required = self::OPTIONAL;
-	/** @var Google_Service_Plus_Person|array An array of infos (provided by Google)
+	/** @var array An array of infos (provided by Google)
 	 * about a user. */
 	public $userInfo;
 
@@ -30,7 +30,7 @@ class GoogleUserInfoAuthenticationRequest extends AuthenticationRequest {
 		return [
 			'provider' => wfMessage( 'googlelogin-auth-service-name' ),
 			'account' =>
-				$googleUser ? new \RawMessage( '$1', [ $googleUser->getFullNameWithId() ] ) :
+				$googleUser ? new \RawMessage( '$1', [ $googleUser->getEmailWithId() ] ) :
 					wfMessage( 'googlelogin-auth-service-unknown-account' )
 		];
 	}
