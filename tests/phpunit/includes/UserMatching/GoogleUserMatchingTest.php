3d2e633e07d2d03b8eb839574bd1155e4242978d
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2018-07-21 18:29:20 +0200

    Match users by their emial-address, too
    Additionally to matching the user when manually connected a MediaWiki
    account and a Google account, GoogleLogin will, with this change, also
    match the Accounts based on the e-mail-address provided from Google
    after the sucessful login and given consent. The following things need
    to be true for this to work:
    
     * The E-Mail address returned from Google needs to be the E-Mail in the
       MediaWiki account to match
     * The E-Mail address returned from Google must not be assigned to multiple
       MediaWiki accounts
     * The MediaWiki account e-mail address needs to be verified
    
    Bug: T200000
    Change-Id: I3fa6e4d92a07cf04ab1d35c1a0c3f1cfcc3d9463

---
+++ tests/phpunit/includes/UserMatching/GoogleUserMatchingTest.php
@@ -0,0 +1,220 @@
+<?php
+
+use GoogleLogin\UserMatching\GoogleUserMatching;
+use MediaWiki\MediaWikiServices;
+
+class GoogleUserMatchingTest extends MediaWikiTestCase {
+	/**
+	 * @var PHPUnit_Framework_MockObject_MockObject
+	 */
+	private $dbConnection;
+	private $loadBalancer;
+	/**
+	 * @var User
+	 */
+	private $loggedInUser;
+
+	private $validToken = [
+		'sub' => '123',
+		'email' => 'test@example.com',
+		'email_verified' => true,
+	];
+
+	protected function setUp() {
+		parent::setUp();
+
+		$this->dbConnection = $this->getMockBuilder( Database::class )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$this->loadBalancer = $this->getMockBuilder( LoadBalancer::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$this->loadBalancer->method( 'getConnection' )->willReturn( $this->dbConnection );
+
+		$this->loggedInUser = User::newFromId( 100 );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenEmptyArray() {
+		$matchingService =
+			new GoogleUserMatching( MediaWikiServices::getInstance()->getDBLoadBalancer() );
+
+		$this->assertNull( $matchingService->getUserFromToken( [] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenTokenNotAssociated() {
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->willReturn( false );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertNull( $matchingService->getUserFromToken( $this->validToken ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenNoEmailAttribute() {
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->willReturn( false );
+		$this->dbConnection->expects( $this->never() )->method( 'select' );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertNull( $matchingService->getUserFromToken( [ 'sub' => '123' ] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenEmailNotVerified() {
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->willReturn( false );
+		$this->dbConnection->expects( $this->never() )->method( 'select' );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$token = $this->validToken;
+		$token['email_verified'] = false;
+		$this->assertNull( $matchingService->getUserFromToken( $token ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenOneEmailLinked() {
+		$aResult = new StdClass();
+		$aResult->user_id = 1;
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->willReturn( false );
+		$this->dbConnection->expects( $this->once() )
+			->method( 'select' )
+			->with(
+				'user',
+				[ 'user_id' ],
+				[ 'user_email' => 'test@example.com', 'user_email_authenticated IS NOT NULL' ]
+			)
+			->willReturn( new FakeResultWrapper( [ $aResult ] ) );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$user = $matchingService->getUserFromToken( $this->validToken );
+		$this->assertEquals( 1, $user->getId() );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenMultipleEmailsLinked() {
+		$aResult = new StdClass();
+		$aResult->user_id = 1;
+		$anotherResult = new StdClass();
+		$anotherResult->user_id = 1;
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->willReturn( false );
+		$this->dbConnection->expects( $this->once() )
+			->method( 'select' )
+			->with(
+				'user',
+				[ 'user_id' ],
+				[ 'user_email' => 'test@example.com', 'user_email_authenticated IS NOT NULL' ]
+			)
+			->willReturn( new FakeResultWrapper( [ $aResult, $anotherResult ] ) );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertNull( $matchingService->getUserFromToken( $this->validToken ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::getUserFromToken()
+	 */
+	public function testGetUserFromTokenTokenAssociated() {
+		$userConnection = new StdClass();
+		$userConnection->user_id = 100;
+
+		$this->dbConnection->expects( $this->once() )
+			->method( 'selectRow' )
+			->with( $this->anything(), $this->anything(), [ 'user_googleid' => '123' ] )
+			->willReturn( $userConnection );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$user = $matchingService->getUserFromToken( $this->validToken );
+
+		$this->assertEquals( 100, $user->getId() );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::match()
+	 */
+	public function testMatchAnonymousUser() {
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertFalse( $matchingService->match( new User(), [] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::match()
+	 */
+	public function testMatchWithoutTokenId() {
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertFalse( $matchingService->match( $this->loggedInUser, [] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::match()
+	 */
+	public function testMatchWithTokenIdAndUser() {
+		$this->dbConnection->expects( $this->once() )
+			->method( 'insert' )
+			->with( 'user_google_user', [
+				'user_id' => 100,
+				'user_googleid' => '123'
+			] )
+			->willReturn( true );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertTrue( $matchingService->match( $this->loggedInUser, $this->validToken ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::unmatch()
+	 */
+	public function testUnmatchAnonymousUser() {
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertFalse( $matchingService->unmatch( new User(), [] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::unmatch()
+	 */
+	public function testUnmatchWithoutTokenId() {
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertFalse( $matchingService->unmatch( $this->loggedInUser, [] ) );
+	}
+
+	/**
+	 * @covers \GoogleLogin\UserMatching\GoogleUserMatching::unmatch()
+	 */
+	public function testUnmatchWithTokenIdAndUser() {
+		$this->dbConnection->expects( $this->once() )
+			->method( 'delete' )
+			->with( 'user_google_user', [
+				'user_id' => 100,
+				'user_googleid' => '123'
+			] )
+			->willReturn( true );
+		$matchingService = new GoogleUserMatching( $this->loadBalancer );
+
+		$this->assertTrue( $matchingService->unmatch( $this->loggedInUser, $this->validToken ) );
+	}
+}
