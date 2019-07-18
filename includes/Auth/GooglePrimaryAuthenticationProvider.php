645ef4e023fe55c05d0f1cfeb7b258762566f0b0
Florian <florian.schmidt.stargatewissen@gmail.com>
2019-07-18 19:23:08 +0200

    Autocreate accounts when configured and the only auth provider
    GoogleLogin now supports to autocreate accounts if one logs in and
    there's no associated account already. This requires the wiki to be
    setup very strictly:
    
     * GoogleLogin needs to be the only primary authentication provider
     * The @ sign needs to be whitelisted in the
       $wgInvalidUsernameCharacters setting
     * Autocreation of accounts needs to be enabled:
       $wgGroupPermissions['*']['autocreateaccount'] = true;
     * The GoogleLogin authoritative mode needs to be enabled
    
    This will turn GoogleLogin into the authoritative mode, in which it will
    automatically create a user if there's no connected user already, which
    will result in a login action to always succeed if the Google login
    worked. This also has the following influence:
    
     * The username of the account is the primary e-mail field of the
       returned information from Google. This is not configurable for now
     * If a local wiki account is already connected, this account is used to
       login, which means that an already existing account takes precedence
       over creating a new one
     * The new created account will automatically being linked with the
       Google account, which makes turning off this feature a bit easier.
     * GoogleLogin will not let a user add or remove any further Google
       account connections (or the automatically created link) to prevent a
       user from being not able to login anymore
    
    Bug: T186080
    Change-Id: Ic9f8e43ff3660580ec4b32d99ee84a789ff25ba6

--- includes/Auth/GooglePrimaryAuthenticationProvider.php
+++ includes/Auth/GooglePrimaryAuthenticationProvider.php
@@ -6,22 +6,19 @@
 namespace GoogleLogin\Auth;
 
 use Exception;
-
 use GoogleLogin\Constants;
 use GoogleLogin\GoogleIdProvider;
+use GoogleLogin\GoogleLogin;
 use GoogleLogin\GoogleUserMatching;
-use MediaWiki\Auth\AuthenticationRequest;
 use MediaWiki\Auth\AbstractPrimaryAuthenticationProvider;
-use MediaWiki\Auth\AuthManager;
+use MediaWiki\Auth\AuthenticationRequest;
 use MediaWiki\Auth\AuthenticationResponse;
+use MediaWiki\Auth\AuthManager;
 use MediaWiki\MediaWikiServices;
 use MWException;
-use User;
-
-use GoogleLogin\GoogleLogin;
-
-use StatusValue;
 use SpecialPage;
+use StatusValue;
+use User;
 
 /**
  * Implements a primary authentication provider to authenticate an user using a Google account where
@@ -37,18 +34,20 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	const TOKEN_SALT = 'GooglePrimaryAuthenticationProvider:redirect';
 	/** Name of the button of the GoogleAuthenticationRequest */
 	const GOOGLELOGIN_BUTTONREQUEST_NAME = 'googlelogin';
+	/** @var string Session data key to identify a saved Google ID token between requests */
+	const GOOGLE_ACCOUNT_TOKEN_KEY = 'googlelogin:account:token';
 
 	public function beginPrimaryAuthentication( array $reqs ) {
 		return $this->beginGoogleAuthentication( $reqs, self::GOOGLELOGIN_BUTTONREQUEST_NAME );
 	}
 
 	public function continuePrimaryAuthentication( array $reqs ) {
-		$request = AuthenticationRequest::getRequestByClass( $reqs,
-			GoogleServerAuthenticationRequest::class );
+		$request =
+			AuthenticationRequest::getRequestByClass( $reqs,
+				GoogleServerAuthenticationRequest::class );
 		if ( !$request ) {
 			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-error-no-authentication-workflow' )
-			);
+				wfMessage( 'googlelogin-error-no-authentication-workflow' ) );
 		}
 
 		try {
@@ -58,54 +57,75 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 			}
 
 			/** @var GoogleUserMatching $userMatchingService */
-			$userMatchingService = MediaWikiServices::getInstance()
-				->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+			$userMatchingService =
+				MediaWikiServices::getInstance()
+					->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
 			$user = $userMatchingService->getUserFromToken( $verifiedToken );
+
+			$email = $verifiedToken['email'];
+			if ( !GoogleLogin::isValidDomain( $email ) ) {
+				return AuthenticationResponse::newFail( wfMessage( 'googlelogin-unallowed-domain',
+					$email ) );
+			}
+
 			if ( $user ) {
-				$email = $verifiedToken['email'];
-				if ( !GoogleLogin::isValidDomain( $email ) ) {
-					return AuthenticationResponse::newFail(
-						wfMessage( 'googlelogin-unallowed-domain', $email )
-					);
-				}
 				return AuthenticationResponse::newPass( $user->getName() );
 			} else {
-				$resp = AuthenticationResponse::newPass( null );
+				$resp = $this->nonExistingUserResponse( $verifiedToken );
 				$resp->linkRequest = new GoogleUserInfoAuthenticationRequest( $verifiedToken );
 				$resp->createRequest = $resp->linkRequest;
+
 				return $resp;
 			}
-		} catch ( Exception $e ) {
-			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-generic-error', $e->getMessage() )
-			);
 		}
+		catch ( Exception $e ) {
+			return AuthenticationResponse::newFail( wfMessage( 'googlelogin-generic-error',
+				$e->getMessage() ) );
+		}
+	}
+
+	public function autoCreatedAccount( $user, $source ) {
+		/** @var GoogleUserMatching $userMatchingService */
+		$userMatchingService =
+			MediaWikiServices::getInstance()->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+
+		$verifiedToken =
+			$this->manager->getAuthenticationSessionData( self::GOOGLE_ACCOUNT_TOKEN_KEY );
+		$userMatchingService->match( $user, $verifiedToken );
+		$this->manager->removeAuthenticationSessionData( self::GOOGLE_ACCOUNT_TOKEN_KEY );
 	}
 
 	public function getAuthenticationRequests( $action, array $options ) {
 		switch ( $action ) {
 			case AuthManager::ACTION_LOGIN:
-				return [ new GoogleAuthenticationRequest(
-					wfMessage( 'googlelogin' ),
-					wfMessage( 'googlelogin-loginbutton-help' )
-				) ];
+				return [
+					new GoogleAuthenticationRequest( wfMessage( 'googlelogin' ),
+						wfMessage( 'googlelogin-loginbutton-help' ) ),
+				];
 				break;
 			case AuthManager::ACTION_LINK:
-				// TODO: Probably not the best message currently.
-				return [ new GoogleAuthenticationRequest(
-					wfMessage( 'googlelogin-form-merge' ),
-					wfMessage( 'googlelogin-link-help' )
-				) ];
+				if ( $this->isAuthoritative() ) {
+					return [];
+				}
+
+				return [
+					new GoogleAuthenticationRequest( wfMessage( 'googlelogin-form-merge' ),
+						wfMessage( 'googlelogin-link-help' ) ),
+				];
 				break;
 			case AuthManager::ACTION_REMOVE:
+				if ( $this->isAuthoritative() ) {
+					return [];
+				}
 				$user = User::newFromName( $options['username'] );
 
 				if ( $user === false || $user->isAnon() ) {
 					return [];
 				}
 				/** @var GoogleIdProvider $googleIdProvider */
-				$googleIdProvider = MediaWikiServices::getInstance()
-					->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
+				$googleIdProvider =
+					MediaWikiServices::getInstance()
+						->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
 				$googleIds = $googleIdProvider->getFromUser( $user );
 
 				if ( !$user || empty( $googleIds ) ) {
@@ -116,6 +136,7 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 				foreach ( $googleIds as $key => $id ) {
 					$reqs[] = new GoogleRemoveAuthenticationRequest( $id );
 				}
+
 				return $reqs;
 				break;
 			case AuthManager::ACTION_CREATE:
@@ -123,10 +144,10 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 				// the things provided by inheriting
 				// ButtonAuthenticationRequest, so probably it's better
 				// to create it's own Request
-				return [ new GoogleAuthenticationRequest(
-					wfMessage( 'googlelogin-create' ),
-					wfMessage( 'googlelogin-link-help' )
-				) ];
+				return [
+					new GoogleAuthenticationRequest( wfMessage( 'googlelogin-create' ),
+						wfMessage( 'googlelogin-link-help' ) ),
+				];
 				break;
 			default:
 				return [];
@@ -141,8 +162,8 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 		$user = \User::newFromName( $username );
 
 		/** @var GoogleIdProvider $googleIdProvider */
-		$googleIdProvider = MediaWikiServices::getInstance()
-			->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
+		$googleIdProvider =
+			MediaWikiServices::getInstance()->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
 
 		return $user && !empty( $googleIdProvider->getFromUser( $user ) );
 	}
@@ -150,29 +171,28 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	public function providerAllowsAuthenticationDataChange(
 		AuthenticationRequest $req, $checkData = true
 	) {
-		if (
-			get_class( $req ) === GoogleRemoveAuthenticationRequest::class &&
-			$req->action === AuthManager::ACTION_REMOVE
-		) {
+		if ( get_class( $req ) === GoogleRemoveAuthenticationRequest::class &&
+			$req->action === AuthManager::ACTION_REMOVE ) {
 			$user = User::newFromName( $req->username );
 			/** @var GoogleIdProvider $googleIdProvider */
-			$googleIdProvider = MediaWikiServices::getInstance()
-				->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
-			if ( $user && in_array( $req->getGoogleId(), $googleIdProvider->getFromUser( $user ) ) ) {
+			$googleIdProvider =
+				MediaWikiServices::getInstance()
+					->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
+			if ( $user &&
+				in_array( $req->getGoogleId(), $googleIdProvider->getFromUser( $user ) ) ) {
 				return StatusValue::newGood();
 			} else {
 				return StatusValue::newFatal( wfMessage( 'googlelogin-change-account-not-linked' ) );
 			}
 		}
 
-		if (
-			get_class( $req ) === GoogleUserInfoAuthenticationRequest::class &&
-			$req->action === AuthManager::ACTION_CHANGE
-		) {
+		if ( get_class( $req ) === GoogleUserInfoAuthenticationRequest::class &&
+			$req->action === AuthManager::ACTION_CHANGE ) {
 			$user = User::newFromName( $req->username );
 			/** @var GoogleUserMatching $userMatchingService */
-			$userMatchingService = MediaWikiServices::getInstance()
-				->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+			$userMatchingService =
+				MediaWikiServices::getInstance()
+					->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
 			$potentialUser = $userMatchingService->getUserFromToken( $req->userInfo );
 			if ( $potentialUser && !$potentialUser->equals( $user ) ) {
 				return StatusValue::newFatal( 'googlelogin-link-other1' );
@@ -183,25 +203,22 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 				return StatusValue::newGood();
 			}
 		}
+
 		return StatusValue::newGood( 'ignored' );
 	}
 
 	public function providerChangeAuthenticationData( AuthenticationRequest $req ) {
 		/** @var GoogleUserMatching $userMatchingService */
-		$userMatchingService = MediaWikiServices::getInstance()
-			->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
-		if (
-			get_class( $req ) === GoogleRemoveAuthenticationRequest::class &&
-			$req->action === AuthManager::ACTION_REMOVE
-		) {
+		$userMatchingService =
+			MediaWikiServices::getInstance()->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+		if ( get_class( $req ) === GoogleRemoveAuthenticationRequest::class &&
+			$req->action === AuthManager::ACTION_REMOVE ) {
 			$user = User::newFromName( $req->username );
 			$userMatchingService->unmatch( $user, [ 'sub' => $req->getGoogleId() ] );
 		}
 
-		if (
-			get_class( $req ) === GoogleUserInfoAuthenticationRequest::class &&
-			$req->action === AuthManager::ACTION_CHANGE
-		) {
+		if ( get_class( $req ) === GoogleUserInfoAuthenticationRequest::class &&
+			$req->action === AuthManager::ACTION_CHANGE ) {
 			$user = User::newFromName( $req->username );
 			$userMatchingService->match( $user, $req->userInfo );
 		}
@@ -216,29 +233,32 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	}
 
 	public function beginPrimaryAccountCreation( $user, $creator, array $reqs ) {
-		$request = AuthenticationRequest::getRequestByClass( $reqs,
-			GoogleUserInfoAuthenticationRequest::class );
+		$request =
+			AuthenticationRequest::getRequestByClass( $reqs,
+				GoogleUserInfoAuthenticationRequest::class );
 
 		/** @var GoogleIdProvider $googleIdProvider */
-		$googleIdProvider = MediaWikiServices::getInstance()
-			->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
+		$googleIdProvider =
+			MediaWikiServices::getInstance()->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
 		if ( $request ) {
 			if ( !$googleIdProvider->isAssociated( $request->userInfo['sub'] ) ) {
 				$resp = AuthenticationResponse::newPass();
 				$resp->linkRequest = $request;
+
 				return $resp;
 			}
 		}
+
 		return $this->beginGoogleAuthentication( $reqs, self::GOOGLELOGIN_BUTTONREQUEST_NAME );
 	}
 
 	public function continuePrimaryAccountCreation( $user, $creator, array $reqs ) {
-		$request = AuthenticationRequest::getRequestByClass( $reqs,
-			GoogleServerAuthenticationRequest::class );
+		$request =
+			AuthenticationRequest::getRequestByClass( $reqs,
+				GoogleServerAuthenticationRequest::class );
 		if ( !$request ) {
 			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-error-no-authentication-workflow' )
-			);
+				wfMessage( 'googlelogin-error-no-authentication-workflow' ) );
 		}
 
 		try {
@@ -248,24 +268,26 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 			}
 
 			/** @var GoogleIdProvider $googleIdProvider */
-			$googleIdProvider = MediaWikiServices::getInstance()
-				->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
+			$googleIdProvider =
+				MediaWikiServices::getInstance()
+					->getService( Constants::SERVICE_GOOGLE_ID_PROVIDER );
 			if ( !$googleIdProvider->isAssociated( $verifiedToken['sub'] ) ) {
 				$email = $verifiedToken['email'];
 				if ( !GoogleLogin::isValidDomain( $email ) ) {
-					return AuthenticationResponse::newFail(
-						wfMessage( 'googlelogin-unallowed-domain', $email )
-					);
+					return AuthenticationResponse::newFail( wfMessage( 'googlelogin-unallowed-domain',
+						$email ) );
 				}
 				$resp = AuthenticationResponse::newPass();
 				$resp->linkRequest = new GoogleUserInfoAuthenticationRequest( $verifiedToken );
+
 				return $resp;
 			}
+
 			return AuthenticationResponse::newFail( wfMessage( 'googlelogin-link-other' ) );
-		} catch ( Exception $e ) {
-			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-generic-error', $e->getMessage() )
-			);
+		}
+		catch ( Exception $e ) {
+			return AuthenticationResponse::newFail( wfMessage( 'googlelogin-generic-error',
+				$e->getMessage() ) );
 		}
 	}
 
@@ -275,8 +297,8 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 		$user->saveSettings();
 
 		/** @var GoogleUserMatching $userMatchingService */
-		$userMatchingService = MediaWikiServices::getInstance()
-			->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+		$userMatchingService =
+			MediaWikiServices::getInstance()->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
 		$userMatchingService->match( $user, $userInfo );
 
 		return null;
@@ -287,12 +309,12 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	}
 
 	public function continuePrimaryAccountLink( $user, array $reqs ) {
-		$request = AuthenticationRequest::getRequestByClass( $reqs,
-			GoogleServerAuthenticationRequest::class );
+		$request =
+			AuthenticationRequest::getRequestByClass( $reqs,
+				GoogleServerAuthenticationRequest::class );
 		if ( !$request ) {
 			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-error-no-authentication-workflow' )
-			);
+				wfMessage( 'googlelogin-error-no-authentication-workflow' ) );
 		}
 		$client = $this->getGoogleClient();
 		$client->fetchAccessTokenWithAuthCode( $request->accessToken );
@@ -304,8 +326,9 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 			}
 
 			/** @var GoogleUserMatching $userMatchingService */
-			$userMatchingService = MediaWikiServices::getInstance()
-				->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
+			$userMatchingService =
+				MediaWikiServices::getInstance()
+					->getService( Constants::SERVICE_GOOGLE_USER_MATCHING );
 			$potentialUser = $userMatchingService->getUserFromToken( $verifiedToken );
 			if ( $potentialUser && !$potentialUser->equals( $user ) ) {
 				return AuthenticationResponse::newFail( wfMessage( 'googlelogin-link-other' ) );
@@ -314,9 +337,8 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 			} else {
 				$email = $verifiedToken['email'];
 				if ( !GoogleLogin::isValidDomain( $email ) ) {
-					return AuthenticationResponse::newFail(
-						wfMessage( 'googlelogin-unallowed-domain', $email )
-					);
+					return AuthenticationResponse::newFail( wfMessage( 'googlelogin-unallowed-domain',
+						$email ) );
 				}
 				$result = $userMatchingService->match( $user, $verifiedToken );
 				if ( $result ) {
@@ -326,10 +348,10 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 					return AuthenticationResponse::newFail( new \RawMessage( 'Database error' ) );
 				}
 			}
-		} catch ( Exception $e ) {
-			return AuthenticationResponse::newFail(
-				wfMessage( 'googlelogin-generic-error', $e->getMessage() )
-			);
+		}
+		catch ( Exception $e ) {
+			return AuthenticationResponse::newFail( wfMessage( 'googlelogin-generic-error',
+				$e->getMessage() ) );
 		}
 	}
 
@@ -342,15 +364,18 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	 * @return AuthenticationResponse
 	 */
 	private function beginGoogleAuthentication( array $reqs, $buttonAuthenticationRequestName ) {
-		$req = GoogleAuthenticationRequest::getRequestByName( $reqs, $buttonAuthenticationRequestName );
+		$req =
+			GoogleAuthenticationRequest::getRequestByName( $reqs,
+				$buttonAuthenticationRequestName );
 		if ( !$req ) {
 			return AuthenticationResponse::newAbstain();
 		}
 		$client = $this->getGoogleClient();
-		$this->manager->setAuthenticationSessionData( self::RETURNURL_SESSION_KEY, $req->returnToUrl );
+		$this->manager->setAuthenticationSessionData( self::RETURNURL_SESSION_KEY,
+			$req->returnToUrl );
 
 		return AuthenticationResponse::newRedirect( [
-			new GoogleServerAuthenticationRequest()
+			new GoogleServerAuthenticationRequest(),
 		], $client->createAuthUrl() );
 	}
 
@@ -360,10 +385,12 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 	 * @return \Google_Client
 	 */
 	public function getGoogleClient() {
-		$client = GoogleLogin::getClient(
-			SpecialPage::getTitleFor( 'GoogleLoginReturn' )->getFullURL( '', false, PROTO_CURRENT ),
-			$this->manager->getRequest()->getSession()->getToken( self::TOKEN_SALT )->toString()
-		);
+		$client =
+			GoogleLogin::getClient( SpecialPage::getTitleFor( 'GoogleLoginReturn' )
+				->getFullURL( '', false, PROTO_CURRENT ), $this->manager->getRequest()
+				->getSession()
+				->getToken( self::TOKEN_SALT )
+				->toString() );
 
 		return $client;
 	}
@@ -379,13 +406,11 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 		if ( !$request->accessToken || $request->errorCode ) {
 			switch ( $request->errorCode ) {
 				case 'access_denied':
-					return AuthenticationResponse::newFail(
-						wfMessage( 'googlelogin-access-denied' ) );
+					return AuthenticationResponse::newFail( wfMessage( 'googlelogin-access-denied' ) );
 					break;
 				default:
-					return AuthenticationResponse::newFail( wfMessage(
-						'googlelogin-generic-error', $request->errorCode ? $request->errorCode :
-						'unknown' ) );
+					return AuthenticationResponse::newFail( wfMessage( 'googlelogin-generic-error',
+						$request->errorCode ? $request->errorCode : 'unknown' ) );
 			}
 		}
 		$client = $this->getGoogleClient();
@@ -397,5 +422,31 @@ class GooglePrimaryAuthenticationProvider extends AbstractPrimaryAuthenticationP
 		}
 
 		return $verifiedToken;
+	}
+
+	/**
+	 * @param $verifiedToken
+	 * @return AuthenticationResponse
+	 */
+	private function nonExistingUserResponse( $verifiedToken ) {
+		if ( $this->isAuthoritative() ) {
+			$this->manager->setAuthenticationSessionData( self::GOOGLE_ACCOUNT_TOKEN_KEY,
+				$verifiedToken );
+			$resp = AuthenticationResponse::newPass( $verifiedToken['email'] );
+		} else {
+			$resp = AuthenticationResponse::newPass( null );
+		}
+
+		return $resp;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	private function isAuthoritative() {
+		return MediaWikiServices::getInstance()
+			->getConfigFactory()
+			->makeConfig( 'googlelogin' )
+			->get( 'GLAuthoritativeMode' );
 	}
 }
---
+++ includes/ConfigurationError.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GoogleLogin;
+
+use MWException;
+
+class ConfigurationError extends MWException {
+}
