a9302a20ae78a7000488e3a828b51e8bfe340808
mainframe98 <k.s.werf@hotmail.com>
2019-09-08 12:22:07 +0200

    Inject ObjectFactory into ApiModuleManager
    The ApiModuleManager now constructs modules through
    the ObjectFactory, which allows it to construct modules
    that require services.
    
    Bug: T222388
    Depends-On: Iee04afc27283547dd68d6db93f44ac2e0ebf1258
    Change-Id: I9ebd28c5ab911223489ea65fbb75f0bd7c0bf62d

--- tests/phpunit/integration/includes/GoogleLoginHooksTest.php
+++ tests/phpunit/integration/includes/GoogleLoginHooksTest.php
@@ -4,6 +4,7 @@ namespace GoogleLogin;
 
 use ApiMain;
 use ApiModuleManager;
+use MediaWiki\MediaWikiServices;
 use GoogleLogin\Auth\GooglePrimaryAuthenticationProvider;
 use MediaWikiIntegrationTestCase;
 
@@ -19,7 +20,10 @@ class GoogleLoginHooksTest extends MediaWikiIntegrationTestCase {
 
 	public function setup() {
 		parent::setUp();
-		$this->moduleManager = new ApiModuleManager( new ApiMain() );
+		$this->moduleManager = new ApiModuleManager(
+			new ApiMain(),
+			MediaWikiServices::getInstance()->getObjectFactory()
+		);
 	}
 
 	/**
